548b45a20e5fc71a86e7902116b5417c82ebaad6
Olivari, Mathieu <mathieu@codeaurora.org>
2012-04-27 21:50:29 -0700

    [ar71xx] Update ALSA files header to ISC
    Change-Id: I61ddd86af48a4d7509eee93d5cfffba940451605

--- target/linux/ar71xx/files/sound/soc/qca/qca-db12x.c
+++ target/linux/ar71xx/files/sound/soc/qca/qca-db12x.c
@@ -1,10 +1,19 @@
 /*
- * ath-db12x.c -- ALSA machine code for DB12x board ref design (and relatives)
+ * qca-db12x.c -- ALSA machine code for DB12x board ref design (and relatives)
  *
- * Author:	Mathieu Olivari
- * Created:	Jan 25, 2011
- * Copyright:	(C) 2011 Qualcomm Atheros, Inc.
+ * Copyright (c) 2012 Atheros Communications Inc.
  *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include <asm/delay.h>
