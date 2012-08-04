8e7946422e4ecfcd402b821ad7ac0d4eeb376141
Olivari, Mathieu <mathieu@codeaurora.org>
2012-08-03 19:17:42 -0700

    [ar71xx] Add --dump-regs support to ath79 MAC drv
    Change-Id: I4984e3b728feba85079a1b2b376305b012338b36
    Signed-off-by: Olivari, Mathieu <mathieu@codeaurora.org>

--- target/linux/ar71xx/files/drivers/net/ethernet/atheros/ag71xx/ag71xx_ethtool.c
+++ target/linux/ar71xx/files/drivers/net/ethernet/atheros/ag71xx/ag71xx_ethtool.c
@@ -112,10 +112,51 @@ static int ag71xx_ethtool_set_ringparam(struct net_device *dev,
 	return err;
 }
 
+static int ag71xx_ethtool_get_regs_len(struct net_device *netdev)
+{
+#define AG71XX_REGS_LEN 23
+	return AG71XX_REGS_LEN * sizeof(u32);
+}
+
+static void ag71xx_ethtool_get_regs(struct net_device *netdev,
+			            struct ethtool_regs *regs, void *p)
+{
+	struct ag71xx *ag = netdev_priv(netdev);
+	u32 *regs_buff = p;
+
+	memset(p, 0, AG71XX_REGS_LEN * sizeof(u32));
+
+	regs_buff[0]  = ag71xx_rr(ag, AG71XX_REG_MAC_CFG1);
+	regs_buff[1]  = ag71xx_rr(ag, AG71XX_REG_MAC_CFG2);
+	regs_buff[2]  = ag71xx_rr(ag, AG71XX_REG_MAC_IPG);
+	regs_buff[3]  = ag71xx_rr(ag, AG71XX_REG_MAC_HDX);
+	regs_buff[4]  = ag71xx_rr(ag, AG71XX_REG_MAC_MFL);
+	regs_buff[5]  = 0xFFFFFFFF;
+	regs_buff[6]  = 0xFFFFFFFF;
+	regs_buff[7]  = 0xFFFFFFFF;
+	regs_buff[8]  = ag71xx_rr(ag, AG71XX_REG_MII_CFG);
+	regs_buff[9]  = 0xFFFFFFFF;
+	regs_buff[10] = 0xFFFFFFFF;
+	regs_buff[11] = 0xFFFFFFFF;
+	regs_buff[12] = 0xFFFFFFFF;
+	regs_buff[13] = 0xFFFFFFFF;
+	regs_buff[14] = ag71xx_rr(ag, AG71XX_REG_MAC_IFCTL);
+	regs_buff[15] = ag71xx_rr(ag, AG71XX_REG_MAC_ADDR1);
+	regs_buff[16] = ag71xx_rr(ag, AG71XX_REG_MAC_ADDR2);
+	regs_buff[17] = ag71xx_rr(ag, AG71XX_REG_FIFO_CFG0);
+	regs_buff[18] = ag71xx_rr(ag, AG71XX_REG_FIFO_CFG1);
+	regs_buff[19] = ag71xx_rr(ag, AG71XX_REG_FIFO_CFG2);
+	regs_buff[20] = ag71xx_rr(ag, AG71XX_REG_FIFO_CFG3);
+	regs_buff[21] = ag71xx_rr(ag, AG71XX_REG_FIFO_CFG4);
+	regs_buff[22] = ag71xx_rr(ag, AG71XX_REG_FIFO_CFG5);
+}
+
 struct ethtool_ops ag71xx_ethtool_ops = {
 	.set_settings	= ag71xx_ethtool_set_settings,
 	.get_settings	= ag71xx_ethtool_get_settings,
 	.get_drvinfo	= ag71xx_ethtool_get_drvinfo,
+	.get_regs_len	= ag71xx_ethtool_get_regs_len,
+	.get_regs	= ag71xx_ethtool_get_regs,
 	.get_msglevel	= ag71xx_ethtool_get_msglevel,
 	.set_msglevel	= ag71xx_ethtool_set_msglevel,
 	.get_ringparam	= ag71xx_ethtool_get_ringparam,
