f49cec151130c688e5f777d0914b75c996a2da00
Michael Larson <mlarson@codeaurora.org>
2012-12-28 17:06:32 -0800

    [Skifta]  Firmware upgrades were broken in a couple of ways.
    1 - The Awk scripts need to check for the variable name at the beginning of the line only
    
    2 - On a firmware update we need to erase the overlay partition of the FS, otherwise changes do not take.
    
    Change-Id: Ib19e263d51f3aa083448f7df421a2871ac526f01
    Signed-off-by: Michael Larson <mlarson@codeaurora.org>
    Reviewed-by: Mathieu Olivari <mathieu@codeaurora.org>

--- target/linux/ar71xx/base-files/lib/upgrade/cus227.sh
+++ target/linux/ar71xx/base-files/lib/upgrade/cus227.sh
@@ -7,9 +7,9 @@ cus227_update_u_boot_env() {
 	v "Updating cus-227 u-boot-env..."
 	[ -x /usr/sbin/fw_printenv -a -x /usr/sbin/fw_setenv ] || { v "fw_printenv or fw_setenv is not executable"; return; }
 	[ -f /etc/fw_env.config ] || { v "/etc/fw_env.config does not exist"; return; }
-	BOOTARGS=$(fw_printenv 2>/dev/null |  awk '/bootargs/' | sed -e 's/bootargs=//' -e 's/(kernel)/(tmp-k-1)/' -e 's/(rootfs)/(tmp-r-1)/' -e 's/(firmware)/(tmp-fw-1)/' -e 's/(k-2)/(kernel)/' -e 's/(r-2)/(rootfs)/' -e 's/(fw-2)/(firmware)/' -e 's/(tmp-k-1)/(k-2)/' -e 's/(tmp-r-1)/(r-2)/' -e 's/(tmp-fw-1)/(fw-2)/')
+	BOOTARGS=$(fw_printenv 2>/dev/null |  awk '/^bootargs/' | sed -e 's/bootargs=//' -e 's/(kernel)/(tmp-k-1)/' -e 's/(rootfs)/(tmp-r-1)/' -e 's/(firmware)/(tmp-fw-1)/' -e 's/(k-2)/(kernel)/' -e 's/(r-2)/(rootfs)/' -e 's/(fw-2)/(firmware)/' -e 's/(tmp-k-1)/(k-2)/' -e 's/(tmp-r-1)/(r-2)/' -e 's/(tmp-fw-1)/(fw-2)/')
 	BOOTCMDADDR=$(echo $BOOTARGS | awk -F ' ' '{for(i=1;i<=NF;i++) {if($i ~ /mtdparts/) print $i}}' | awk -F, '{for(i=1;i<=NF;i++) {if($i ~ /(firmware)/) print $i}}' | sed -e 's/(firmware)//' | awk -F@ '{print $2}')
-	BOOTCMD=$(fw_printenv 2>/dev/null |  awk '/bootcmd/' | sed -e 's/bootcmd=//' -e 's/0[xX][a-fA-F0-9]*$//')
+	BOOTCMD=$(fw_printenv 2>/dev/null |  awk '/^bootcmd/' | sed -e 's/bootcmd=//' -e 's/0[xX][a-fA-F0-9]*$//')
 	echo "	bootargs "$BOOTARGS"
 	bootcmd "$BOOTCMD $BOOTCMDADDR"" | fw_setenv -s -
 	v "done"
@@ -23,5 +23,6 @@ platform_do_upgrade_cus227() {
 		dd bs=512 skip=1 if="$1" | mtd write - fw-2
 	fi
 	cus227_update_u_boot_env
+	mtd erase rootfs_data
 }
 
