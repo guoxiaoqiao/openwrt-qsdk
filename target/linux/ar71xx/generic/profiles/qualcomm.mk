4c37e9ba1ea4dc99071f4521e8d0c5e41edd812d
Mathieu Olivari <mathieu@codeaurora.org>
2013-01-09 20:16:52 -0800

    [ar71xx] Add Skifta as a profile in qualcomm.mk
    Change-Id: Ief626eee1be30f8fc383ada3ad06240a4efc91c5
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>

--- target/linux/ar71xx/generic/profiles/qualcomm.mk
+++ target/linux/ar71xx/generic/profiles/qualcomm.mk
@@ -3,7 +3,7 @@
 #
 
 define Profile/QSDK_Open
-	NAME:=Qualcomm-Atheros Open Profile
+	NAME:=Qualcomm-Atheros SDK Open Profile
 	PACKAGES:=luci uhttpd kmod-usb-core kmod-usb2 kmod-usb-storage -kmod-ath5k
 endef
 
@@ -17,7 +17,7 @@ endef
 $(eval $(call Profile,QSDK_Open))
 
 define Profile/QSDK_Main
-	NAME:=Qualcomm-Atheros Main Profile
+	NAME:=Qualcomm-Atheros SDK Main Profile
 	PACKAGES:=luci uhttpd kmod-usb-core kmod-usb2 kmod-usb-storage kmod-qca-wifi \
 	  qca-hostap qca-hostapd-cli qca-wpa-cli qca-wpa-supplicant qca-legacy-uboot-ap135 \
 	  qca-legacy-uboot-db12x  -kmod-ath9k -kmod-ath5k -kmod-ath -wpad-mini
@@ -31,3 +31,32 @@ define Profile/QSDK_Main/Description
 	-Integrated 11abgn/ac support using the proprietary qca-wifi driver
 endef
 $(eval $(call Profile,QSDK_Main))
+
+define Profile/Skifta
+	NAME:=Qualcomm-Atheros Skifta Profile
+	PACKAGES:=-dropbear -firewall -ppp -wpad-mini alsa -luci-theme-openwrt \
+		kmod-usb-core kmod-usb2 kmod-usb-storage kmod-sound-soc-cus227 \
+		kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat avahi-daemon \
+		hostapd wpa-supplicant hostapd-utils \
+		orb-orbplayer orb-cabdirectory \
+		luci-sam alsa skifta rygel qcom-state-mgr-skifta
+endef
+
+define Profile/Skifta/Description
+	Qualcomm-Atheros Skifta package set configuration
+	This profile provides all the skifta features in the firmware for the
+	Skifta supported boards. It includes
+	- STA mode configuration
+	- Skifta configuration manager
+	- UPnP/DLNA Renderer
+	- Audio drivers
+	- Audio player software
+	- Skifta Engine
+	- Java virtual machine
+
+	This profile integrates skifta specific software components that may not
+	be delivered as part of this release. If any question, please contact
+	the Qualcomm-Atheros sales team for more information on the Skifta
+	products & deliverables.
+endef
+$(eval $(call Profile,Skifta))
