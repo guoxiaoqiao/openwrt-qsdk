55cb17f2885040d78be5d529203214636000612c
Mathieu Olivari <mathieu@codeaurora.org>
2013-01-15 13:20:54 -0800

    [uci] Add uci_add_list() as a shell function
    This function is meant as a shell interface to uci. It's similar to what
    uci_add() is doing for "uci add" or to what uci_set() is doing for "uci set"
    
    Change-Id: I80fdd33b53d1a2a8b33041cb84acc93cdbabf877
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>

--- package/uci/files/lib/config/uci.sh
+++ package/uci/files/lib/config/uci.sh
@@ -85,6 +85,15 @@ uci_set() {
 	/sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} set "$PACKAGE.$CONFIG.$OPTION=$VALUE"
 }
 
+uci_add_list() {
+	local PACKAGE="$1"
+	local CONFIG="$2"
+	local OPTION="$3"
+	local VALUE="$4"
+
+	/sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} add_list "$PACKAGE.$CONFIG.$OPTION=$VALUE"
+}
+
 uci_get_state() {
 	uci_get "$1" "$2" "$3" "$4" "/var/state"
 }
