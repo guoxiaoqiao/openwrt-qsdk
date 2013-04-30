29971d74fa29cad0234458a50573711e364b58ae
Mathieu Olivari <mathieu@codeaurora.org>
2013-04-29 20:06:44 -0700

    [ar71xx] Add QoS support to QSDK profiles (open & main)
    *EV125407: [PVT-QSDK] Doesn't support QoS
    
    Change-Id: Iad446e3d56549b44de6caf60ce84b436bb7b2781
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>

--- target/linux/ar71xx/generic/profiles/qualcomm.mk
+++ target/linux/ar71xx/generic/profiles/qualcomm.mk
@@ -7,7 +7,7 @@ define Profile/QSDK_Open
 	PACKAGES:=luci uhttpd kmod-usb-core kmod-usb2 kmod-usb-storage -kmod-ath5k \
 	  kmod-ipt-nathelper-extra luci-app-upnp tftp-hpa sysstat igmpproxy kmod-ipt-nathelper-rtsp \
 	  kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 \
-	  luci-app-ddns ddns-scripts kmod-ipv6 iwinfo \
+	  luci-app-ddns ddns-scripts kmod-ipv6 iwinfo luci-app-qos \
 	  -wpad-mini hostapd hostapd-utils wpa-supplicant wpa-cli
 endef
 
@@ -25,7 +25,7 @@ define Profile/QSDK_Main
 	PACKAGES:=luci uhttpd kmod-ipt-nathelper-extra luci-app-upnp \
 	  kmod-usb-core kmod-usb2 kmod-usb-storage kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat \
 	  kmod-nls-cp437 kmod-nls-iso8859-1 tftp-hpa sysstat igmpproxy kmod-ipt-nathelper-rtsp \
-	  kmod-ipv6 \
+	  kmod-ipv6 luci-app-qos \
 	  kmod-qca-wifi qca-hostap qca-hostapd-cli qca-wpa-cli qca-wpa-supplicant \
 	  luci-app-ddns ddns-scripts qca-legacy-uboot-ap135 qca-legacy-uboot-db12x \
 	  -kmod-ath9k -kmod-ath5k -kmod-ath -wpad-mini
