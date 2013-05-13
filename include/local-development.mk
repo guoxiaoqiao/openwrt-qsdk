43c3342f80e72f7767c9dcaf337af9aaee5f24c8
Matthew McClintock <mmcclint@codeaurora.org>
2013-05-13 16:07:46 -0500

    [include] fix git describe not prefixing with g
    If there are no tags, git describe --always will not output
    a g prefix before the SHA, and just show the SHA. This is a work
    around to fix that scenario
    
    Change-Id: Id61a3a8f48890ede55ed320cba6af60f94969135
    Signed-off-by: Matthew McClintock <mmcclint@codeaurora.org>
    Reviewed-by: Mathieu Olivari <mathieu@codeaurora.org>

--- include/local-development.mk
+++ include/local-development.mk
@@ -3,7 +3,7 @@
 LOCAL_SRC ?= $(TOPDIR)/qca/src/$(PKG_NAME)
 
 ifeq (exists, $(shell [ -d $(LOCAL_SRC) ] && echo exists))
-PKG_VERSION:=$(shell cd $(LOCAL_SRC)/; git describe --dirty --long --always | sed 's/.*-g/g/g')
+PKG_VERSION:=g$(shell cd $(LOCAL_SRC)/; git describe --dirty --long --always | sed 's/.*-g//g')
 PKG_SOURCE_URL:=
 PKG_UNPACK=mkdir -p $(PKG_BUILD_DIR); $(CP) $(LOCAL_SRC)/* $(PKG_BUILD_DIR)/
 endif
