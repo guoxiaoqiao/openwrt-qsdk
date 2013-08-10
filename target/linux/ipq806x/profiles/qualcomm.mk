6ddf8266a22e2ccec14ad7c78e92302491a7aaa3
Mathieu Olivari <mathieu@codeaurora.org>
2013-08-10 10:34:49 -0700

    [ipq806x] Make QSDK open depend on QSDK base profile
    This will make sure that all the wifi independent packages
    present in the retail/carrier/enterprise profiles are also
    present in the Open profile.
    
    Change-Id: I03c2e13e251263b33b694cf03abe91b9cb5af33d
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>

--- target/linux/ipq806x/profiles/qualcomm.mk
+++ target/linux/ipq806x/profiles/qualcomm.mk
@@ -2,15 +2,19 @@
 # Copyright (c) 2013 Qualcomm Atheros, Inc.
 #
 
+define Profile/QSDK_Base
+	PACKAGES:=luci uhttpd kmod-ipt-nathelper-extra luci-app-upnp kmod-fs-ext4 \
+	  kmod-usb-core kmod-usb2 kmod-usb-storage kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat \
+	  kmod-nls-cp437 kmod-nls-iso8859-1 tftp-hpa sysstat igmpproxy kmod-ipt-nathelper-rtsp \
+	  kmod-ipv6 luci-app-qos kmod-ebtables iperf devmem2 ip ethtool \
+	  quagga quagga-ripd quagga-zebra quagga-watchquagga rp-pppoe-relay \
+	  -dnsmasq dnsmasq-dhcpv6 radvd wide-dhcpv6-client luci-app-samba bridge \
+	  luci-app-ddns ddns-scripts uboot-ipq806x-cdp
+endef
+
 define Profile/QSDK_Open
 	NAME:=Qualcomm-Atheros SDK Open Profile
-	PACKAGES:=luci uhttpd kmod-usb-core kmod-usb2 kmod-usb-storage -kmod-ath5k \
-	  kmod-ipt-nathelper-extra luci-app-upnp tftp-hpa sysstat igmpproxy kmod-ipt-nathelper-rtsp \
-	  kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 \
-	  luci-app-ddns ddns-scripts kmod-ipv6 iwinfo luci-app-qos \
-	  quagga quagga-ripd quagga-zebra quagga-watchquagga rp-pppoe-relay \
-	  -dnsmasq dnsmasq-dhcpv6 radvd wide-dhcpv6-client \
-	  -wpad-mini hostapd hostapd-utils wpa-supplicant wpa-cli wireless-tools
+	PACKAGES+=kmod-ath9k wpad-mini
 endef
 
 define Profile/QSDK_Open/Description
@@ -22,21 +26,10 @@ define Profile/QSDK_Open/Description
 endef
 $(eval $(call Profile,QSDK_Open))
 
-define Profile/QSDK_Base
-	PACKAGES:=luci uhttpd kmod-ipt-nathelper-extra luci-app-upnp kmod-fs-ext4 \
-	  kmod-usb-core kmod-usb2 kmod-usb-storage kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat \
-	  kmod-nls-cp437 kmod-nls-iso8859-1 tftp-hpa sysstat igmpproxy kmod-ipt-nathelper-rtsp \
-	  kmod-ipv6 luci-app-qos kmod-ebtables iperf devmem2 ip ethtool \
-	  quagga quagga-ripd quagga-zebra quagga-watchquagga rp-pppoe-relay \
-	  kmod-qca-wifi qca-hostap qca-hostapd-cli qca-wpa-cli qca-wpa-supplicant \
-	  -dnsmasq dnsmasq-dhcpv6 radvd wide-dhcpv6-client luci-app-samba bridge \
-	  luci-app-ddns ddns-scripts qca-legacy-uboot-ap135 qca-legacy-uboot-db12x uboot-ipq806x-cdp \
-	  -kmod-ath9k -kmod-ath5k -kmod-ath -wpad-mini
-endef
-
 define Profile/QSDK_Retail
 	$(Profile/QSDK_Base)
 	NAME:=Qualcomm-Atheros SDK Retail Profile
+	PACKAGES+=kmod-qca-wifi qca-hostap qca-hostapd-cli qca-wpa-cli qca-wpa-supplicant
 endef
 
 define Profile/QSDK_Retail/Description
@@ -51,7 +44,7 @@ $(eval $(call Profile,QSDK_Retail))
 define Profile/QSDK_Carrier
 	$(Profile/QSDK_Base)
 	NAME:=Qualcomm-Atheros SDK Carrier Profile
-	PACKAGES+=
+	PACKAGES+=kmod-qca-wifi qca-hostap qca-hostapd-cli qca-wpa-cli qca-wpa-supplicant
 endef
 
 define Profile/QSDK_Carrier/Description
@@ -62,7 +55,8 @@ $(eval $(call Profile,QSDK_Carrier))
 define Profile/QSDK_Enterprise
 	$(Profile/QSDK_Base)
 	NAME:=Qualcomm-Atheros SDK Enterprise Profile
-	PACKAGES+=kmod-openswan openswan kmod-crypto-ocf
+	PACKAGES+=kmod-qca-wifi qca-hostap qca-hostapd-cli qca-wpa-cli qca-wpa-supplicant \
+	  kmod-openswan openswan kmod-crypto-ocf
 endef
 
 define Profile/QSDK_Enterprise/Description
