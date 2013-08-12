779e1c356653301ca7863cb9e5a97968121263b5
Mathieu Olivari <mathieu@codeaurora.org>
2013-08-12 01:11:42 -0700

    [ipq806x] Add usb host controller support in QSDK profiles
    This change adds the xhci, dwc3 and usb printer packages into the
    QSDK based profiles.
    
    Change-Id: I4c27d0fe78fac72074124df21d4b7258fdfd7d1e
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>

--- target/linux/ipq806x/profiles/qualcomm.mk
+++ target/linux/ipq806x/profiles/qualcomm.mk
@@ -6,6 +6,8 @@ define Profile/QSDK_Base
 	PACKAGES:=luci uhttpd kmod-ipt-nathelper-extra luci-app-upnp kmod-fs-ext4 \
 	  kmod-usb-core kmod-usb2 kmod-usb-storage kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat \
 	  kmod-sound-core kmod-sound-soc-ipq806x alsa \
+	  kmod-usb-xhci kmod-usb-printer \
+	  kmod-usb-gadget kmod-usb-gadget-dwc3 kmod-usb-gadget-dwc3-ipq \
 	  kmod-nls-cp437 kmod-nls-iso8859-1 tftp-hpa sysstat igmpproxy kmod-ipt-nathelper-rtsp \
 	  kmod-ipv6 luci-app-qos iperf devmem2 ip ethtool ip6tables \
 	  quagga quagga-ripd quagga-zebra quagga-watchquagga rp-pppoe-relay \
