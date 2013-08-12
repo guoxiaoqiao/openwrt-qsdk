f021f8a10837f06f8ee8effc6371a6d71d6e3661
Mathieu Olivari <mathieu@codeaurora.org>
2013-08-12 05:53:40 -0700

    [ipq806x] Add audio packages to QSDK profiles
    This adds the IPQ soundcard driver and the alsa & gstreamer packages
    to the QSDK profile.
    
    Change-Id: Ic369f447cf97d2da7818abe2a3bc199e064a4afd
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>

--- target/linux/ipq806x/profiles/qualcomm.mk
+++ target/linux/ipq806x/profiles/qualcomm.mk
@@ -5,6 +5,7 @@
 define Profile/QSDK_Base
 	PACKAGES:=luci uhttpd kmod-ipt-nathelper-extra luci-app-upnp kmod-fs-ext4 \
 	  kmod-usb-core kmod-usb2 kmod-usb-storage kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat \
+	  kmod-sound-core kmod-sound-soc-ipq806x alsa \
 	  kmod-nls-cp437 kmod-nls-iso8859-1 tftp-hpa sysstat igmpproxy kmod-ipt-nathelper-rtsp \
 	  kmod-ipv6 luci-app-qos iperf devmem2 ip ethtool ip6tables \
 	  quagga quagga-ripd quagga-zebra quagga-watchquagga rp-pppoe-relay \
