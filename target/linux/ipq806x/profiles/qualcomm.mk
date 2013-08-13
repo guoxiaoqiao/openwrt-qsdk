f7643b1b690f443bb0be0a98a4f831795b19aaca
Mathieu Olivari <mathieu@codeaurora.org>
2013-08-12 21:59:40 -0700

    [ipq806x] Add ip6tables to all QSDK profiles
    This module will add IPv6 support to netfilter by adding the
    corresponding options to the kernel. It is required so that
    the NSS hardware can accelerate IPv6 traffic.
    
    This change also adds a config options in
    target/linux/generic/config-3.4 that is present in msm-3.4 kernel
    and need to be defined so the user doesn't get prompted during the
    build. This option will be disabled by default.
    
    Change-Id: Iaec5e5cecc24c7e44c79d0ce3212cc8001c1d017
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>
    Reviewed-by: Matthew Mcclintock <mmcclint@codeaurora.org>

--- target/linux/ipq806x/profiles/qualcomm.mk
+++ target/linux/ipq806x/profiles/qualcomm.mk
@@ -6,7 +6,7 @@ define Profile/QSDK_Base
 	PACKAGES:=luci uhttpd kmod-ipt-nathelper-extra luci-app-upnp kmod-fs-ext4 \
 	  kmod-usb-core kmod-usb2 kmod-usb-storage kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat \
 	  kmod-nls-cp437 kmod-nls-iso8859-1 tftp-hpa sysstat igmpproxy kmod-ipt-nathelper-rtsp \
-	  kmod-ipv6 luci-app-qos iperf devmem2 ip ethtool \
+	  kmod-ipv6 luci-app-qos iperf devmem2 ip ethtool ip6tables \
 	  quagga quagga-ripd quagga-zebra quagga-watchquagga rp-pppoe-relay \
 	  -dnsmasq dnsmasq-dhcpv6 radvd wide-dhcpv6-client luci-app-samba bridge \
 	  luci-app-ddns ddns-scripts uboot-ipq806x-cdp
