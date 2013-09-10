92c3fe3bca50f51a59068f246c9fef38065a10d1
Mathieu Olivari <mathieu@codeaurora.org>
2013-09-09 17:01:08 -0700

    hostapd: add external registrar support
    Setting wireless.@wifi-iface[N].ext_registrar=1 will enable UPNP
    advertising and add an external registrar to the interface this vif
    belongs to (br-lan if the vif is included in the LAN bridge). By
    enabling this we append upnp_iface=xxx to the hostapd config file.
    
    Change-Id: I4426c4de418e8587ab39e7eaf70cab08a0f23961
    Signed-off-by: Sujith Manoharan <c_manoha@codeaurora.org>
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>
    Signed-off-by: Luis R. Rodriguez <rodrigue@codeaurora.org>

--- package/hostapd/files/hostapd.sh
+++ package/hostapd/files/hostapd.sh
@@ -204,6 +204,9 @@ hostapd_set_bss_options() {
 		config_get_bool ext_registrar "$vif" ext_registrar 0
 		[ "$ext_registrar" -gt 0 -a -n "$bridge" ] && append "$var" "upnp_iface=$bridge" "$N"
 
+		config_get_bool ext_registrar "$vif" ext_registrar 0
+		[ "$ext_registrar" -gt 0 -a -n "$bridge" ] && append "$var" "upnp_iface=$bridge" "$N"
+
 		append "$var" "eap_server=1" "$N"
 		append "$var" "ap_pin=$wps_pin" "$N"
 		append "$var" "wps_state=${wps_not_configured:-2}" "$N"
