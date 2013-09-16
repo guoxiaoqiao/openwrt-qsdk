8526c3cf19fb83201294ebfda1304e15d5bc4a4f
Mathieu Olivari <mathieu@codeaurora.org>
2013-09-16 10:35:22 -0700

    [ipq806x] Add tracepath/tracepath6 to profiles
    These tools are required for IPv4/IPv6 test purpose.
    
    CR-Fixed: 545526
    Change-Id: I1ac791473d9b97334030d54a314e956eee0e9ffe
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>

--- target/linux/ipq806x/profiles/qualcomm.mk
+++ target/linux/ipq806x/profiles/qualcomm.mk
@@ -12,7 +12,8 @@ define Profile/QSDK_Base
 	  kmod-ipv6 luci-app-qos iperf devmem2 ip ethtool ip6tables \
 	  quagga quagga-ripd quagga-zebra quagga-watchquagga rp-pppoe-relay \
 	  -dnsmasq dnsmasq-dhcpv6 radvd wide-dhcpv6-client luci-app-samba bridge \
-	  luci-app-ddns ddns-scripts uboot-ipq806x-cdp cups
+	  luci-app-ddns ddns-scripts uboot-ipq806x-cdp cups \
+	  iputils-tracepath iputils-tracepath6
 endef
 
 define Profile/QSDK_Open
