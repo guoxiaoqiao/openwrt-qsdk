a9a616ffe2011616f00e6538edea76cb580c9570
Mathieu Olivari <mathieu@codeaurora.org>
2013-12-08 17:24:40 +0545

    [ar71xx] Update ar71xx profiles to reflect latest expectations
    ar71xx profiles are legacy and not adapter to the products. This change
    removes the existing profiles and replaces them by the newly specified
    ones:
    *Wired Router (AP136)
    *Premium Router (AP135, AP135-NAND)
    
    Change-Id: Ie1231bfe51ad179babbbb40d2955d506e18bea18
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>

--- target/linux/ar71xx/generic/profiles/qualcomm.mk
+++ target/linux/ar71xx/generic/profiles/qualcomm.mk
@@ -2,96 +2,55 @@
 # Copyright (c) 2013 The Linux Foundation. All rights reserved.
 #
 
-define Profile/QSDK_Open
-	NAME:=Qualcomm-Atheros SDK Open Profile
-	PACKAGES:=luci uhttpd kmod-usb-core kmod-usb2 kmod-usb-storage -kmod-ath5k \
-	  kmod-ipt-nathelper-extra luci-app-upnp tftp-hpa sysstat igmpproxy kmod-ipt-nathelper-rtsp \
-	  kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 \
-	  luci-app-ddns ddns-scripts kmod-ipv6 iwinfo luci-app-qos \
-	  quagga quagga-ripd quagga-zebra quagga-watchquagga rp-pppoe-relay \
-	  -dnsmasq dnsmasq-dhcpv6 radvd wide-dhcpv6-client \
-	  -wpad-mini hostapd hostapd-utils wpa-supplicant wpa-cli wireless-tools
-endef
-
-define Profile/QSDK_Open/Description
-	QSDK Open package set configuration.
-	This profile includes only open source packages and provides basic Wi-Fi router features using the QCA upstream Linux Wi-Fi driver. It supports:
-	-Bridging and routing networking
-	-LuCI web configuration interface
-	-Integrated 11abgn support using the ath9k driver
-endef
-$(eval $(call Profile,QSDK_Open))
-
-define Profile/QSDK_Retail
-	NAME:=Qualcomm-Atheros SDK Retail Profile
-	PACKAGES:=luci uhttpd kmod-ipt-nathelper-extra luci-app-upnp \
-	  kmod-usb-core kmod-usb2 kmod-usb-storage kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat \
+define Profile/QSDK_Base
+	PACKAGES:=luci uhttpd kmod-ipt-nathelper-extra luci-app-upnp kmod-fs-ext4 \
+	  kmod-usb-storage kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat \
+	  ntfs-3g dosfsck e2fsprogs fdisk mkdosfs \
 	  kmod-nls-cp437 kmod-nls-iso8859-1 tftp-hpa sysstat igmpproxy kmod-ipt-nathelper-rtsp \
-	  kmod-ipv6 luci-app-qos kmod-art2-4.9-scorpion qca-spectral \
+	  kmod-ipv6 luci-app-qos iperf devmem2 ip ethtool ip6tables \
 	  quagga quagga-ripd quagga-zebra quagga-watchquagga rp-pppoe-relay \
-	  kmod-qca-wifi qca-hostap qca-hostapd-cli qca-wpa-cli qca-wpa-supplicant \
-	  -dnsmasq dnsmasq-dhcpv6 radvd wide-dhcpv6-client \
-	  luci-app-ddns ddns-scripts qca-legacy-uboot-ap135 qca-legacy-uboot-db12x \
-	  -kmod-ath9k -kmod-ath5k -kmod-ath -wpad-mini hyfi
+	  -dnsmasq dnsmasq-dhcpv6 radvd wide-dhcpv6-client luci-app-samba bridge \
+	  luci-app-ddns ddns-scripts cups cups-client \
+	  iputils-tracepath iputils-tracepath6 alljoyn
 endef
 
-define Profile/QSDK_Retail/Description
-	QSDK Retail package set configuration.
-	This profile provides basic Wi-Fi router features using the QCA proprietary Wi-Fi driver. It supports:
-	-Bridging and routing networking
-	-LuCI web configuration interface
-	-Integrated 11abgn/ac support using the proprietary qca-wifi driver
-endef
-$(eval $(call Profile,QSDK_Retail))
-
-define Profile/QSDK_Carrier
-	# based on retail profile
-	$(Profile/QSDK_Retail)
-	NAME:=Qualcomm-Atheros SDK Carrier Profile
-	PACKAGES+=streamboost hyfi
+define Profile/QSDK_Wired_Router
+	$(Profile/QSDK_Base)
+	NAME:=Qualcomm-Atheros SDK Wired Router Profile
+	PACKAGES+=-kmod-ath9k -kmod-ath5k -kmod-ath -wpad-mini
 endef
 
-define Profile/QSDK_Carrier/Description
-	QSDK Enterprise package set configuration.
+define Profile/QSDK_Wired_Router/Description
+  QSDK Wired Router package set configuration.
+  This profile is designed to fit in a 8M flash and supports the following features:
+  - Bridging and routing networking
+  - LuCI web configuration interface
+  - USB hard drive support
+  - Samba
+  - IPv4/IPv6
+  - DynDns
+  It doens't provide any WiFi driver.
 endef
-$(eval $(call Profile,QSDK_Carrier))
+$(eval $(call Profile,QSDK_Wired_Router))
 
-define Profile/QSDK_Enterprise
-	# based on carrier profile
-	$(Profile/QSDK_Carrier)
-	NAME:=Qualcomm-Atheros SDK Enterprise Profile
-	PACKAGES+=kmod-ipsec kmod-ipsec4 kmod-ipsec6 ipsec-tools
-endef
-
-define Profile/QSDK_Enterprise/Description
-	QSDK Enterprise package set configuration.
-endef
-$(eval $(call Profile,QSDK_Enterprise))
-
-define Profile/Factory/Default
-	PACKAGES:=-dropbear -firewall -ppp -wpad-mini alsa -luci-theme-openwrt \
-		-kmod-ath9k -kmod-ath5k -kmod-ath -hostapd -wpa-supplicant \
-		-hostapd-utils kmod-usb-core kmod-usb2 kmod-usb-storage \
-		kmod-nls-cp437 kmod-nls-iso8859-1 tftp-hpa dumpregs \
-		kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat avahi-daemon
+define Profile/QSDK_Premium_Router
+	$(Profile/QSDK_Base)
+	NAME:=Qualcomm-Atheros SDK Premium Router Profile
+	PACKAGES+= streamboost \
+	  kmod-qca-wifi qca-hostap qca-hostapd-cli qca-wpa-cli qca-wpa-supplicant
 endef
 
-define Profile/Factory/Description/Default
-	Qualcomm-Atheros Factory package set configuration
-	This profile provides the package set required to run factory focus
-	tools. In supports the following:
-	- ART2 (customized for a certain chip)
-	- USB tools
+define Profile/QSDK_Premium_Router/Description
+  QSDK Premium Router package set configuration.
+  This profile is designed to fit in a 16M flash and supports the following features:
+  - Bridging and routing networking
+  - QCA-WiFi driver configuration
+  - LuCI web configuration interface
+  - Streamboost
+  - USB hard drive support
+  - Samba
+  - IPv4/IPv6
+  - DynDns
 endef
+$(eval $(call Profile,QSDK_Premium_Router))
 
-define Profile/Factory_Wasp
-	NAME:=Qualcomm-Atheros Factory Profile for Wasp chipset
-	$(call Profile/Factory/Default)
-	PACKAGES+=kmod-art2-wasp qca-romboot-cus227
-endef
-
-define Profile/Factory_Wasp/Description
-	$(call Profile/Factory/Description/Default)
-	This profile contains the ART2 build for Wasp based platform
-endef
-$(eval $(call Profile,Factory_Wasp))
