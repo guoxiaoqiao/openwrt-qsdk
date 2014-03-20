72e00cfa9ca1839c18ca02daad168adf8fc38d11
Miaoqing Pan <miaoqing@codeaurora.org>
2014-03-20 11:25:27 +0800

    [ag71xx] separate qca953x from ar934x
    Change-Id: I4912812f4719cbeaf6dcc86d686bbc8e5005b26e
    Signed-off-by: Miaoqing Pan <miaoqing@codeaurora.org>

--- target/linux/ar71xx/files/arch/mips/ath79/dev-eth.c
+++ target/linux/ar71xx/files/arch/mips/ath79/dev-eth.c
@@ -196,6 +196,7 @@ void ath79_init_mdio_pdata(unsigned int id, u32 phy_mask)
 	case ATH79_SOC_AR7241:
 	case ATH79_SOC_AR9330:
 	case ATH79_SOC_AR9331:
+	case ATH79_SOC_QCA9531:
 		mdio_dev = &ath79_mdio1_device;
 		mdio_data = &ath79_mdio1_data;
 		break;
@@ -238,6 +239,7 @@ void ath79_init_mdio_pdata(unsigned int id, u32 phy_mask)
 		mdio_data->is_ar9330 = 1;
 		/* fall through */
 	case ATH79_SOC_AR9331:
+	case ATH79_SOC_QCA9531:
 		mdio_data->builtin_switch = 1;
 		break;
 
@@ -543,6 +545,7 @@ static void __init ath79_init_eth_pll_data(unsigned int id)
 	case ATH79_SOC_AR9341:
 	case ATH79_SOC_AR9342:
 	case ATH79_SOC_AR9344:
+	case ATH79_SOC_QCA9531:
 	case ATH79_SOC_QCA9558:
 		pll_10 = AR934X_PLL_VAL_10;
 		pll_100 = AR934X_PLL_VAL_100;
@@ -622,6 +625,7 @@ static int __init ath79_setup_phy_if_mode(unsigned int id,
 		case ATH79_SOC_AR7241:
 		case ATH79_SOC_AR9330:
 		case ATH79_SOC_AR9331:
+		case ATH79_SOC_QCA9531:
 			pdata->phy_if_mode = PHY_INTERFACE_MODE_MII;
 			break;
 
@@ -691,6 +695,7 @@ static int __init ath79_setup_phy_if_mode(unsigned int id,
 		case ATH79_SOC_AR9341:
 		case ATH79_SOC_AR9342:
 		case ATH79_SOC_AR9344:
+		case ATH79_SOC_QCA9531:
 			switch (pdata->phy_if_mode) {
 			case PHY_INTERFACE_MODE_MII:
 			case PHY_INTERFACE_MODE_GMII:
@@ -935,6 +940,7 @@ void ath79_init_eth_pdata(unsigned int id)
 	case ATH79_SOC_AR9341:
 	case ATH79_SOC_AR9342:
 	case ATH79_SOC_AR9344:
+	case ATH79_SOC_QCA9531:
 		if (id == 1) {
 			pdata->switch_data = &ath79_switch_data;
 
@@ -1004,6 +1010,7 @@ void ath79_init_eth_pdata(unsigned int id)
 		case ATH79_SOC_AR7241:
 		case ATH79_SOC_AR9330:
 		case ATH79_SOC_AR9331:
+		case ATH79_SOC_QCA9531:
 			pdata->mii_bus_dev = &ath79_mdio1_device.dev;
 			break;
 
