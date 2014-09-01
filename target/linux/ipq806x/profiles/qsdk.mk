527bdcf7c0ec57290a99ff71f162781029c574b1
Mohammed Shafi Shajakhan <mohammed@codeaurora.org>
2014-09-01 11:33:03 +0530

    [ipq806x] Enable qca-wrapd by default
    Change-Id: I02a958dd885a2abc6908da1fd3869f0584475e95
    Signed-off-by: Mohammed Shafi Shajakhan <mohammed@codeaurora.org>

--- target/linux/ipq806x/profiles/qsdk.mk
+++ target/linux/ipq806x/profiles/qsdk.mk
@@ -20,7 +20,7 @@ define Profile/QSDK_Base
 	  kmod-art2 file pure-ftpd kmod-qca-nss-qdisc xl2tpd ppp-mod-pptp flock pm-utils \
 	  kmod-qca-nss-macsec kmod-qca-wifi-akronite-perf qca-hostap qca-hostapd-cli \
 	  qca-wpa-supplicant-macsec qca-wpa-cli qca-spectral qca-wapid sigma-dut qca-acfg \
-	  qca-wifi-fw
+	  qca-wrapd qca-wifi-fw
 endef
 
 define Profile/QSDK_Open
