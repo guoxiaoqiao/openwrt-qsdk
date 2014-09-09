606a04dd383a4ec089215ff85b5b6d9f1653324c
Matthew McClintock <mmcclint@codeaurora.org>
2014-09-09 12:05:00 -0500

    [ipq806x] add some packages for IPQ
    This brings us closer to QSDK 2.0 package set
    
    Change-Id: I06a3d33a1aa73b55f62645f43aa7e5b101adab7a
    Signed-off-by: Matthew McClintock <mmcclint@qca.qualcomm.com>

--- target/linux/ipq806x/profiles/qsdk.mk
+++ target/linux/ipq806x/profiles/qsdk.mk
@@ -18,10 +18,21 @@ STORAGE:=kmod-scsi-core kmod-usb-storage \
 	kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat \
 	kmod-nls-cp437 kmod-nls-iso8859-1
 
+# TODO: trim to separate packages for features, or cdrouter, etc
+QSDK_BASE:=kmod-ipt-nathelper-extra luci-app-upnp kmod-fs-ext4 \
+          kmod-ipt-ipopt kmod-ipt-conntrack-qos kmod-nls-cp437 kmod-nls-iso8859-1 \
+	  tftp-hpa sysstat mcproxy kmod-ipt-nathelper-rtsp \
+          kmod-ipv6 iperf devmem2 ip ethtool ip6tables ds-lite \
+          quagga quagga-ripd quagga-zebra quagga-watchquagga quagga-vtysh rp-pppoe-relay \
+          -dnsmasq dnsmasq-dhcpv6 radvd wide-dhcpv6-client bridge \
+          luci-app-ddns ddns-scripts mdadm\
+          iputils-tracepath iputils-tracepath6 \
+          file pure-ftpd xl2tpd ppp-mod-pptp flock pm-utils \
+
 define Profile/QSDK_Standard
 	NAME:=Qualcomm-Atheros SDK Standard Profile
 	PACKAGES:=$(OPENWRT_STANDARD) $(NSS_STANDARD) $(SSDK_PKGS) \
-		$(WIFI_OPEN_PKGS) $(STORAGE)
+		$(WIFI_OPEN_PKGS) $(STORAGE) $(QSDK_BASE)
 endef
 
 define Profile/QSDK_Standard/Description
