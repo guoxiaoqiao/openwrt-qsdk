fdaa57cf475027711eb1accc8738173f820d8b6f
Mohammed Shafi Shajakhan <mohammed@codeaurora.org>
2014-09-19 15:42:28 +0530

    [include] support custom source directory for kernel packages
    Also support package version override for the same
    
    Change-Id: I5f2c5efce8bb105b7b50f058b533b5cbf693e2df
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>
    Signed-off-by: Mohammed Shafi Shajakhan <mohammed@codeaurora.org>

--- include/package-version-override.mk
+++ include/package-version-override.mk
@@ -1,3 +1,4 @@
+# Copyright (c) 2013 The Linux Foundation. All rights reserved.
 # remember the provided package version
 PKG_VERSION_ORGINAL:=$(PKG_VERSION)
 
@@ -5,22 +6,39 @@ PKG_VERSION_ORGINAL:=$(PKG_VERSION)
 ifeq ($(CONFIG_$(PKG_NAME)_USE_CUSTOM_VERSION),y)
 PKG_VERSION:=$(call qstrip,$(CONFIG_$(PKG_NAME)_CUSTOM_VERSION))
 PKG_SOURCE:=$(subst $(PKG_VERSION_ORGINAL),$(PKG_VERSION),$(PKG_SOURCE))
+PKG_BUILD_DIR:=$(subst $(PKG_VERSION_ORGINAL),$(PKG_VERSION),$(PKG_BUILD_DIR))
 PKG_MD5SUM:=
 endif
 
 # package specific configuration
 # if includeded the package version can be overwritten within the .config file (instead of changing the package specific Makefile)
+# $(1): package name
+# $(2): list of selected versions. if missing, will use an input string
 define Package/$(PKG_NAME)/override_version
 	menu "overwrite package version"
-		depends on PACKAGE_$(PKG_NAME)
+		depends on PACKAGE_$(1)
 	config $(PKG_NAME)_USE_CUSTOM_VERSION
-		depends on PACKAGE_$(PKG_NAME)
+		depends on PACKAGE_$(1)
 		bool "Use custom package version"
 		default n
+$(if $(2),
+	choice
+		prompt "select version"
+		depends on $(PKG_NAME)_USE_CUSTOM_VERSION
+	$(foreach version,$(2),
+		config $(1)_VERSION_$(version)
+			bool "$(version)")
+	endchoice
+	config $(PKG_NAME)_CUSTOM_VERSION
+		string
+	$(foreach version,$(2),
+		default "$(version)" if $(1)_VERSION_$(version))
+,
 	config $(PKG_NAME)_CUSTOM_VERSION
 		depends on $(PKG_NAME)_USE_CUSTOM_VERSION
-		string "$(PKG_BASE_NAME) version as string (default version: $(PKG_VERSION_ORGINAL))"
+		string "$(PKG_NAME) version as string (default version: $(PKG_VERSION_ORGINAL))"
 		default "$(PKG_VERSION_ORGINAL)"
+)
 	endmenu
 endef
 
@@ -34,9 +52,9 @@ endif
 # instead of using a source ball (eg tar.gz) the specified path will point to the location of the sources
 define Package/$(PKG_NAME)/override_source_path
 	menu "custom source directory"
-		depends on PACKAGE_$(PKG_NAME)
+		depends on PACKAGE_$(1)
 	config $(PKG_NAME)_USE_CUSTOM_SOURCE_DIR
-		depends on PACKAGE_$(PKG_NAME)
+		depends on PACKAGE_$(1)
 		bool "Use custom source directory"
 		default n
 	config $(PKG_NAME)_CUSTOM_SOURCE_DIR
@@ -50,8 +68,13 @@ endef
 # include both configurations as long this file is included before package.mk
 # in case that you're defining your own onfiguration within the package Makefile just include the stuff by yourself
 define Package/$(PKG_NAME)/config
-   $(call Package/$(PKG_NAME)/override_version)
-   $(call Package/$(PKG_NAME)/override_source_path)
+   $(call Package/$(PKG_NAME)/override_version,$(PKG_NAME))
+   $(call Package/$(PKG_NAME)/override_source_path,$(PKG_NAME))
+endef
+
+define KernelPackage/$(PKG_NAME)/config
+   $(call Package/$(PKG_NAME)/override_version,kmod-$(PKG_NAME))
+   $(call Package/$(PKG_NAME)/override_source_path,kmod-$(PKG_NAME))
 endef
 
 # hook for custom source path
