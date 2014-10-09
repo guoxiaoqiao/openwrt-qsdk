fa2ac87c11057d13ba58939d613fc91bfb40a51a
Mohammed Riyaz Piyarajohn <mpiyaraj@codeaurora.org>
2014-10-09 22:48:17 +0530

    [ipq806x] Enable hardware PRNG support
    1. Updated kernel config to enable the msm-rng driver
    2. Enabled the rng-tools package (to feed entropy to /dev/urandom)
    
    Change-Id: I1951d4ba7ba77e1f01610cb6db7e2527cd5780a8
    CRs-Fixed: 736422
    Signed-off-by: Mohammed Riyaz Piyarajohn <mpiyaraj@codeaurora.org>

--- target/linux/ipq806x/profiles/qsdk.mk
+++ target/linux/ipq806x/profiles/qsdk.mk
@@ -18,7 +18,7 @@ define Profile/QSDK_Base
 	  alljoyn-notification alljoyn-services_common \
 	  kmod-qca-ssdk-nohnat qca-ssdk-shell \
 	  file pure-ftpd kmod-qca-nss-qdisc xl2tpd ppp-mod-pptp flock pm-utils \
-	  kmod-qca-nss-macsec qca-thermald
+	  kmod-qca-nss-macsec qca-thermald rng-tools
 endef
 
 PACKAGES_WIFI_10_2:=kmod-qca-wifi-akronite-perf kmod-art2 qca-hostap qca-hostapd-cli \
