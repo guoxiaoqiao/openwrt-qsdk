85aaf65e8654175c8f724113363baaada4e427e7
Srinivas dodda <doddas@codeaurora.org>
2014-11-02 15:14:58 -0800

    remove the old FW dependency
    Change-Id: I686fe11f973aed33c296767d8077217d12f44a5c
    Signed-off-by: Srinivas dodda <doddas@codeaurora.org>

--- target/linux/ipq806x/profiles/qsdk.mk
+++ target/linux/ipq806x/profiles/qsdk.mk
@@ -22,9 +22,9 @@ define Profile/QSDK_Base
 endef
 
 PACKAGES_WIFI_10_2:=kmod-qca-wifi-akronite-perf kmod-art2 qca-hostap qca-hostapd-cli \
-	  qca-wpa-cli qca-spectral qca-wapid sigma-dut qca-acfg qca-wrapd qca-wifi-fw \
+	  qca-wpa-cli qca-spectral qca-wapid sigma-dut qca-acfg qca-wrapd \
 
-PACKAGES_WIFI_10_4:=qca-wifi-fw-10.4-asic qcmbr-10.4 qca-acfg-10.4 \
+PACKAGES_WIFI_10_4:=qcmbr-10.4 qca-acfg-10.4 \
 	qca-wrapd-10.4 qca-wpc-10.4 whc kmod-qca-wifi-10.4-akronite-perf \
 	kmod-art2-10.4 qca-hostap-10.4 qca-hostapd-cli-10.4 \
 	qca-wpa-cli-10.4 qca-spectral-10.4 qca-wapid-10.4 sigma-dut-10.4 \
