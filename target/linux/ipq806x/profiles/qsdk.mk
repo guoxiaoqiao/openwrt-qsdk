33e3972d49fb592e58af1e47cd69db41a075b6dc
Shyam Sunder <ssunde@codeaurora.org>
2015-02-19 19:57:38 +0530

    openwrt: Enable packages for AQ PHY Driver and MUSIC 8511 Switch driver for AP160.2
    CRs-Fixed: 795674
    
    Change-Id: Iaa8bab5a4a4ff5b6f240983f8f05717faa420701
    Signed-off-by: Shyam Sunder <ssunde@codeaurora.org>

--- target/linux/ipq806x/profiles/qsdk.mk
+++ target/linux/ipq806x/profiles/qsdk.mk
@@ -18,7 +18,8 @@ define Profile/QSDK_Base
 	  alljoyn-notification alljoyn-services_common \
 	  kmod-qca-ssdk-nohnat qca-ssdk-shell \
 	  file pure-ftpd kmod-qca-nss-drv-qdisc xl2tpd ppp-mod-pptp flock pm-utils \
-	  kmod-qca-nss-macsec qca-thermald rng-tools perf kmod-nat-sctp
+	  kmod-qca-nss-macsec qca-thermald rng-tools perf kmod-nat-sctp kmod-aq_phy \
+	  kmod-qca_85xx_sw
 endef
 
 PACKAGES_WIFI_10_2:=kmod-qca-wifi-akronite-perf kmod-art2 qca-hostap qca-hostapd-cli \
