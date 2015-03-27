2ec7689017f313a56c63582065baa729244ee989
Naresh Mehta <naresh@codeaurora.org>
2015-03-27 21:38:20 +0530

    [ipq806x] : Support for calculating correct CPU affinity upto 3 radios
    Changed logic to calculate SMP affinity to support 3rd radio. In case of 3 radio
    design,
    1. First radio will not be assigned to any CPU affinity,
    2. Second radio will be assigned to CPU1 and
    3. Third radio will be assigned to CPU0
    
    For 2 radio design, no change(i.e. first radio to CPU1 and second radio to CPU0).
    
    IRs-Fixed: IR-066790
    
    Change-Id: I492ae8f8acdf482f151cbcbdfe5dd7df69b9dd65
    Signed-off-by: Naresh Mehta <naresh@codeaurora.org>

--- target/linux/ipq806x/base-files/lib/update_smp_affinity.sh
+++ target/linux/ipq806x/base-files/lib/update_smp_affinity.sh
@@ -6,11 +6,13 @@
 enable_smp_affinity_wifi() {
 	local device="$1"
 	local radioidx=${device#wifi}
+	local max_radio_supported=3
 
 	no_of_cpu=`grep processor /proc/cpuinfo | wc -l`
-	smp_affinity=`expr $no_of_cpu - $radioidx - 1`
+	no_of_radio=`ls -l /sys/class/net/wifi* | wc -l`
+	smp_affinity=`expr $no_of_cpu - $radioidx + $no_of_radio - $max_radio_supported`
 
-	if [ "$no_of_cpu" -ge 2 -a "$smp_affinity" -ge 0 ]; then
+	if [ "$no_of_cpu" -ge 2 -a "$smp_affinity" -ge 0 -a "$smp_affinity" -lt "$no_of_cpu" ]; then
 		let "smp_affinity = 1 << $smp_affinity"
 		irq_affinity_num=`grep $device /proc/interrupts | cut -d ':' -f 1 | tr -d ' '`
 		[ -n "$irq_affinity_num" ] && echo $smp_affinity > /proc/irq/$irq_affinity_num/smp_affinity
