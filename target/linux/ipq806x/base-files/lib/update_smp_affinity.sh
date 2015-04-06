f3c162d169a64e8c0bbd6ca21cd4bc09b962de87
Mohammed Shafi (Mohammed Shafi) Shajakhan <mohammed@codeaurora.org>
2015-04-06 20:41:42 +0530

    ipq806x: Update smp affinity based on device capabilities
    Assign Core 1 for pure 11ac device, Core 0 for everything
    else.
    
    Change-Id: Ibb22cb486410d43ec0a4bf601207fcf71e641d9c
    Signed-off-by: Mohammed Shafi (Mohammed Shafi) Shajakhan <mohammed@codeaurora.org>

--- target/linux/ipq806x/base-files/lib/update_smp_affinity.sh
+++ target/linux/ipq806x/base-files/lib/update_smp_affinity.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 #
-# Copyright (c) 2014 The Linux Foundation. All rights reserved.
+# Copyright (c) 2015 The Linux Foundation. All rights reserved.
 # Copyright (C) 2011 OpenWrt.org
 
 enable_smp_affinity_wifi() {
 	local device="$1"
-	local radioidx=${device#wifi}
+	local hwcaps smp_affinity=1
 
-	no_of_cpu=`grep processor /proc/cpuinfo | wc -l`
-	smp_affinity=`expr $no_of_cpu - $radioidx - 1`
+	hwcaps=$(cat /sys/class/net/$device/hwcaps)
+	irq_affinity_num=`grep $device /proc/interrupts | cut -d ':' -f 1 | tr -d ' '`
 
-	if [ "$no_of_cpu" -ge 2 -a "$smp_affinity" -ge 0 ]; then
-		let "smp_affinity = 1 << $smp_affinity"
-		irq_affinity_num=`grep $device /proc/interrupts | cut -d ':' -f 1 | tr -d ' '`
-		[ -n "$irq_affinity_num" ] && echo $smp_affinity > /proc/irq/$irq_affinity_num/smp_affinity
-	fi
+	case "${hwcaps}" in
+		*11an/ac)
+			smp_affinity=2
+		;;
+	esac
+
+	[ -n "$irq_affinity_num" ] && echo $smp_affinity > /proc/irq/$irq_affinity_num/smp_affinity
 }
