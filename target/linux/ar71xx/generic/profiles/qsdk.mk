ea49e89a449eca7874f5c08c8d6e7d1d5e69b771
Miaoqing Pan <miaoqing@codeaurora.org>
2015-04-07 10:48:52 +0800

    ar71xx: add IoE device profile in qsdk.mk
    Change-Id: I57ec27e3e61f222b3f1d1d752e80fdb5e12269c4
    Signed-off-by: Miaoqing Pan <miaoqing@codeaurora.org>

---
+++ target/linux/ar71xx/generic/profiles/qsdk.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2015 The Linux Foundation. All rights reserved.
+#
+
+define Profile/QSDK_IoE_Device
+	NAME:=Qualcomm-Atheros SDK IoE Device Profile
+	PACKAGES:=luci uhttpd kmod-ipt-nathelper-extra luci-app-upnp \
+	  tftp-hpa mcproxy kmod-ipt-nathelper-rtsp kmod-ipv6 \
+	  -dnsmasq dnsmasq-dhcpv6 radvd wide-dhcpv6-client bridge \
+	  sysstat iperf devmem2 ip ethtool ip6tables
+
+	PACKAGES+= -kmod-ath9k -kmod-ath5k -kmod-ath -wpad-mini \
+	  alljoyn alljoyn-about alljoyn-c alljoyn-config alljoyn-controlpanel \
+	  alljoyn-notification alljoyn-services_common \
+	  hostapd hostapd-utils iwinfo  wpa-supplicant-p2p wpa-cli wireless-tools \
+	  kmod-qca-ath10k kmod-qca-ath9k kmod-qca-ath kmod-usb2 kmod-i2c-gpio-custom \
+	  qca-legacy-uboot-ap143-16M qca-legacy-uboot-ap143-32M \
+	  qca-legacy-uboot-cus531-16M qca-legacy-uboot-cus531-dual \
+	  qca-legacy-uboot-cus531-nand qca-legacy-uboot-cus531-32M \
+	  mtd-utils mtd-utils-nandwrite
+endef
+
+define Profile/QSDK_IoE_Device/Description
+	QSDK IoE Device package set configuration.
+	This profile is designed to fit in a 16M flash and supports the following features:
+	- QCA-WiFi driver configuration
+	- IPv4/IPv6
+	- AllJoyn
+endef
+$(eval $(call Profile,QSDK_IoE_Device))
