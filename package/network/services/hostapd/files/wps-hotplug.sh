426a83428d8ba3acda72f6b3ae1f0a3306f22cd5
Miaoqing Pan <miaoqing@codeaurora.org>
2015-05-12 15:50:54 +0800

    hostapd: add WPS pbc script to support STA mode
    Pick the commit from 03881abfe7e859a1d53bcfab474ea4ff556a6ba6.
    
    Change-Id: I48348bc2c4a0095b98a10dd65bd549a0194fb329
    Signed-off-by: Miaoqing Pan <miaoqing@codeaurora.org>
    Signed-off-by: Dominik Sliwa <dsliwa@codeaurora.org>
    Signed-off-by: Marek Kwaczynski <mkwacz@codeaurora.org>

--- package/network/services/hostapd/files/wps-hotplug.sh
+++ package/network/services/hostapd/files/wps-hotplug.sh
@@ -6,6 +6,10 @@ if [ "$ACTION" = "pressed" -a "$BUTTON" = "wps" ]; then
 		[ -S "$socket" ] || continue
 		hostapd_cli -i "$socket" wps_pbc
 	done
+	for dir in /var/run/wpa_supplicant-*; do
+		[ -d "$dir" ] || continue
+		wpa_cli -p "$dir" wps_pbc
+	done
 fi
 
 if [ "$ACTION" = "released" -a "$BUTTON" = "wps" ]; then
