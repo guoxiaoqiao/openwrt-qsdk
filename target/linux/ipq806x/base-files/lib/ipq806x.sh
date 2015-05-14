c4f9f820d47b545af419f2db7a2912d17fc79551
Harsha Chaitanya <hcmana@codeaurora.org>
2015-05-14 20:49:32 +0530

    ipq40xx: Adding board support for DK01.1 and DK04.1
    Populating board name, default network configurations
    and uboot environment for DK01.1 and DK04.1.
    
    Change-Id: I85a6caefa6fee12b9b711a5a0cc5b63edcca0388
    Signed-off-by: Harsha Chaitanya <hcmana@codeaurora.org>

--- target/linux/ipq806x/base-files/lib/ipq806x.sh
+++ target/linux/ipq806x/base-files/lib/ipq806x.sh
@@ -29,6 +29,12 @@ ipq806x_board_detect() {
 	*"WHIRLWIND")
 		name="whirlwind"
 		;;
+	*"AP-DK01.1")
+		name="ap-dk01.1"
+		;;
+	*"AP-DK04.1")
+		name="ap-dk04.1"
+		;;
 	esac
 
 	[ -z "$name" ] && name="unknown"
