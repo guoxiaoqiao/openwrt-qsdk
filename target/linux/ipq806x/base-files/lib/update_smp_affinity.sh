bf66ba82063ddaa4da11daeeba27af0a4fc06daa
Mohammed Shafi Shajakhan <mohammed@codeaurora.org>
2015-06-29 11:08:56 +0530

    ipq806x: Enable wifi smp affinity for qca-wifi with Dakota
    *Add support to enable wifi smp affinity for qca-wifi
    *Assign Core 3, 4 for wifi for all Dakota board variants
    
    Change-Id: I91452a5d6dc703ac405a27d02c45ff6b82a1fa1f
    Signed-off-by: Mohammed Shafi Shajakhan <mohammed@codeaurora.org>

--- target/linux/ipq806x/base-files/lib/update_smp_affinity.sh
+++ target/linux/ipq806x/base-files/lib/update_smp_affinity.sh
@@ -3,7 +3,36 @@
 # Copyright (c) 2015 The Linux Foundation. All rights reserved.
 # Copyright (C) 2011 OpenWrt.org
 
+. /lib/ipq806x.sh
+
 enable_smp_affinity_wifi() {
         irq_wifi=`grep -m2 ath10k_pci /proc/interrupts | cut -d ':' -f 1 | tail -n1 | tr -d ' '`
-	[ -n "$irq_wifi" ] && echo 2 > /proc/irq/$irq_wifi/smp_affinity
+
+	# Enable smp_affinity for ath10k driver
+	if [ -n "$irq_wifi" ]; then
+		echo 2 > /proc/irq/$irq_wifi/smp_affinity
+	else
+	# Enable smp_affinity for qca-wifi driver
+		board=$(ipq806x_board_name)
+		device="$1"
+		hwcaps=$(cat /sys/class/net/$device/hwcaps)
+		irq_affinity_num=`grep $device /proc/interrupts | cut -d ':' -f 1 | tr -d ' '`
+
+		case "${hwcaps}" in
+			*11an/ac)
+				smp_affinity=2
+			;;
+			*)
+				smp_affinity=1
+		esac
+
+		case "$board" in
+			ap-dk0*)
+				# Assign Core 3 (or) 4 for Dakota based on hwcaps
+				smp_affinity=$(($smp_affinity << 2))
+			;;
+		esac
+
+		[ -n "$irq_affinity_num" ] && echo $smp_affinity > /proc/irq/$irq_affinity_num/smp_affinity
+	fi
 }
