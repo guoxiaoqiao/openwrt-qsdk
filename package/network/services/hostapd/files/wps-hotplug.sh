c5db953d4d3cbfa286eadcaa27d46b0f52f61d37
Parameshwaran Kanagaraj <pkanag@codeaurora.org>
2015-09-22 12:51:28 +0530

    hostapd: Implement factory reset for WPS button
    Added support to factory reset operation when WPS push button is pressed for
    more than 3 seconds.
    
    Change-Id: I4f4ccfeefd45ff69a7bc921930345241355f683d
    Signed-off-by: Parameshwaran Kanagaraj <pkanag@codeaurora.org>

--- package/network/services/hostapd/files/wps-hotplug.sh
+++ package/network/services/hostapd/files/wps-hotplug.sh
@@ -8,4 +8,11 @@ if [ "$ACTION" = "pressed" -a "$BUTTON" = "wps" ]; then
 	done
 fi
 
+if [ "$ACTION" = "released" -a "$BUTTON" = "wps" ]; then
+	if [ "$SEEN" -gt 3 ]; then
+		echo "FACTORY RESET" > /dev/console
+		jffs2reset -y && reboot &
+	fi
+fi
+
 return 0
