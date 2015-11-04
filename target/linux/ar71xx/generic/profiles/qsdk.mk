7510c69ed2636565fd14144be6c002bf58f37c5d
xiaofeis <xiaofeis@codeaurora.org>
2015-11-04 14:32:09 +0800

    ar71xx: Enable HNAT on premium profile
    Enable HNAT on premium profile.
    
    Change-Id: Ie82e8a20d2fd02bd0572456a3d84503f93575afa
    Signed-off-by: xiaofeis <xiaofeis@codeaurora.org>

--- target/linux/ar71xx/generic/profiles/qsdk.mk
+++ target/linux/ar71xx/generic/profiles/qsdk.mk
@@ -59,7 +59,7 @@ define Profile/QSDK_Wireless_Router
 	$(Profile/QSDK_Base)
 	NAME:=Qualcomm-Atheros SDK Wireless Router Profile
 	PACKAGES+=-kmod-ath9k -kmod-ath5k -kmod-ath -wpad-mini luci-app-qos \
-	  qca-legacy-uboot-ap136 kmod-qca-ssdk-hnat qca-ssdk-shell kmod-shortcut-fe-cm \
+	  qca-legacy-uboot-ap136 kmod-qca-ssdk-nohnat qca-ssdk-shell kmod-shortcut-fe-cm \
 	  kmod-qca-wifi-perf qca-hostap qca-spectral qca-hostapd-cli qca-wpa-supplicant \
 	  qca-wpa-cli qca-wrapd qca-wapid qca-acfg qca-legacy-uboot-ap152-8M \
 	  qca-legacy-uboot-ap151-8M qca-legacy-uboot-ap147-8M qca-legacy-uboot-db12x
@@ -84,7 +84,7 @@ define Profile/QSDK_Premium_Router
 	NAME:=Qualcomm-Atheros SDK Premium Router Profile
 	PACKAGES+= -kmod-ath9k -kmod-ath5k -kmod-ath -wpad-mini luci-app-samba \
 	  streamboost kmod-fast-classifier $(STORAGE) $(PACKAGES_WIFI_10_2) \
-	  qca-legacy-uboot-ap135 qca-legacy-uboot-ap152-16M kmod-qca-ssdk-nohnat \
+	  qca-legacy-uboot-ap135 qca-legacy-uboot-ap152-16M kmod-qca-ssdk-hnat \
 	  qca-ssdk-shell qca-legacy-uboot-ap147-16M qca-legacy-uboot-ap151-16M \
 	  mtd-utils mtd-utils-nandwrite qca-legacy-uboot-ap135-nand
 endef
