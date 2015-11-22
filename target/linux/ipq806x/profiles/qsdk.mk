4134b4d76f528b9e6c38291dc121fe1212a6f807
Lisa Mitchell <LisaMitchell@codeaurora.org>
2015-11-22 15:42:15 -0800

    Reomve Cascade/Beeliner FW from qsdk.mk
    Change-Id: If6b15d75efd385ae01ec175a5aa3b0e60c5c7fd2
    Signed-off-by: Lisa Mitchell <LisaMitchell@codeaurora.org>

--- target/linux/ipq806x/profiles/qsdk.mk
+++ target/linux/ipq806x/profiles/qsdk.mk
@@ -32,7 +32,6 @@ WIFI_OPEN_PKGS:= kmod-ath9k kmod-ath10k wpad hostapd-utils \
 
 WIFI_10_4_PKGS:=kmod-qca-wifi-10.4-dakota-perf qca-wifi-fw-hw5-10.4-asic \
 	qca-hostap-10.4 qca-hostapd-cli-10.4 qca-wpa-supplicant-10.4 \
-	qca-wifi-fw-hw3-10.4-asic qca-wifi-fw-hw6-10.4-asic \
 	qca-wpa-cli-10.4 qca-spectral-10.4 qca-wpc-10.4 sigma-dut-10.4 \
 	qcmbr-10.4 qca-wrapd-10.4 qca-wapid qca-acfg-10.4 whc whc-ui
 
