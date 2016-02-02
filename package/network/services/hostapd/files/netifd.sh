openwrt-mirror/openwrt

wpa-supplicant: add 802.11r client support
Add 802.11r client support to wpa_supplicant. It's only enabled in
wpa_supplicant-full. hostapd gained 802.11r support in commit r45051.

Tested on a TP-Link TL-WR710N sta psk client with two 802.11r enabled
openwrt accesspoints (TP-Link TL-WDR3600).

Signed-off-by: Stefan Hellermann <stefan@the2masters.de>

git-svn-id: svn://svn.openwrt.org/openwrt/trunk@46377 3c298f89-4303-0410-b956-a3cf2f4a3e73

package/network/services/hostapd/files/netifd.sh
@@ -538,7 +538,9 @@ wpa_supplicant_add_network() {
 	json_get_vars \
 		ssid bssid key \
 		basic_rate mcast_rate \
-		ieee80211w
+		ieee80211w ieee80211r
+
+	set_default ieee80211r 0
 
 	local key_mgmt='NONE'
 	local enc_str=
@@ -548,6 +550,8 @@ wpa_supplicant_add_network() {
 	local wpa_key_mgmt="WPA-PSK"
 	local scan_ssid="scan_ssid=1"
 	local freq
+
+	[ "$ieee80211r" -gt 0 ] && wpa_key_mgmt="FT-PSK $wpa_key_mgmt"
 
 	[[ "$_w_mode" = "adhoc" ]] && {
 		append network_data "mode=1" "$N$T"
@@ -594,6 +598,7 @@ wpa_supplicant_add_network() {
 		;;
 		eap)
 			key_mgmt='WPA-EAP'
+		        [ "$ieee80211r" -gt 0 ] && key_mgmt="FT-EAP $key_mgmt"
 
 			json_get_vars eap_type identity ca_cert
 			[ -n "$ca_cert" ] && append network_data "ca_cert=\"$ca_cert\"" "$N$T"

guoxiaoqiao/openwrt-qsdk

Change-Id: Ic4ec0798d082503a24ea83a9b1dd3a80db8750ff
Signed-off-by: Peter Oh <poh@codeaurora.org>

package/network/services/hostapd/files/netifd.sh
@@ -582,7 +582,9 @@ wpa_supplicant_add_network() {
 		ssid bssid key basic_rate mcast_rate ieee80211w \
 		wps_device_type wps_device_name wps_manufacturer \
 		wps_config wps_model_name wps_model_number \
-		wps_serial_number
+		wps_serial_number ieee80211r
+
+	set_default ieee80211r 0
 
 	local key_mgmt='NONE'
 	local enc_str=
@@ -592,6 +594,8 @@ wpa_supplicant_add_network() {
 	local wpa_key_mgmt="WPA-PSK"
 	local scan_ssid="scan_ssid=1"
 	local freq
+
+	[ "$ieee80211r" -gt 0 ] && wpa_key_mgmt="FT-PSK $wpa_key_mgmt"
 
 	[[ "$_w_mode" = "adhoc" ]] && {
 		append network_data "mode=1" "$N$T"
@@ -638,6 +642,7 @@ wpa_supplicant_add_network() {
 		;;
 		eap)
 			key_mgmt='WPA-EAP'
+		        [ "$ieee80211r" -gt 0 ] && key_mgmt="FT-EAP $key_mgmt"
 
 			json_get_vars eap_type identity ca_cert
 			[ -n "$ca_cert" ] && append network_data "ca_cert=\"$ca_cert\"" "$N$T"