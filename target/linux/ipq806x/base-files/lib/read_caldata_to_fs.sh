c67edcd7196a9d80ec2545bae371095762e49d91
Vivek Bhatt <vsbhat@codeaurora.org>
2016-04-06 14:56:42 +0530

    ipq806x: Retrieve calibration data from FLASH memory properly
    Modified the scripts to retrieve the calibration data from host flash
    memory.
    
    Change-Id: I2727096d606afd483879b8ad73da9f39463ac7bc
    Signed-off-by: Vivek Bhatt <vsbhat@codeaurora.org>

--- target/linux/ipq806x/base-files/lib/read_caldata_to_fs.sh
+++ target/linux/ipq806x/base-files/lib/read_caldata_to_fs.sh
@@ -26,6 +26,12 @@ do_load_ipq4019_board_bin()
                     dd if=${mtdblock} of=${apdk}/wifi0.caldata bs=32 count=377 skip=128
                     dd if=${mtdblock} of=${apdk}/wifi1.caldata bs=32 count=377 skip=640
             ;;
+            ap16* | ap148*)
+                    mkdir -p ${apdk}
+                    dd if=${mtdblock} of=${apdk}/wifi0.caldata bs=32 count=377 skip=128
+                    dd if=${mtdblock} of=${apdk}/wifi1.caldata bs=32 count=377 skip=640
+                    dd if=${mtdblock} of=${apdk}/wifi2.caldata bs=32 count=377 skip=1152
+            ;;
     esac
 }
 
