bdcafae8aa46756b1a6047ef2d4839f68a8619aa
Anto Norbert <norbrt@codeaurora.org>
2016-05-18 14:43:49 +0530

    iptables: Add mark2prio extension
    This patch ports the mark2prio extension module from the banana
    branch to the new iptables package feed.
    
    Change-Id: Ib77ee9bc309c608c9a186dfd07ce29c7fb712104
    Signed-off-by: Anto Norbert <norbrt@codeaurora.org>

--- package/kernel/linux/modules/netfilter.mk
+++ package/kernel/linux/modules/netfilter.mk
@@ -400,6 +400,19 @@ endef
 
 $(eval $(call KernelPackage,ipt-u32))
 
+define KernelPackage/ipt-mark2prio
+  TITLE:=mark2prio support
+  KCONFIG:= CONFIG_NETFILTER_XT_TARGET_MARK2PRIO
+  FILES:= $(LINUX_DIR)/net/netfilter/xt_mark2prio.ko
+  AUTOLOAD:= $(call AutoLoad,50,xt_mark2prio)
+  $(call AddDepends/ipt)
+endef
+
+define KernelPackage/ipt-mark2prio/description
+  Kernel modules for copying mark to priority
+endef
+
+$(eval $(call KernelPackage,ipt-mark2prio))
 
 define KernelPackage/ipt-iprange
   TITLE:=Module for matching ip ranges
