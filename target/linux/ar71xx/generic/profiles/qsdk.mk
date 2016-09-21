4dcfa91f78007bb865268d10c5a3ace23e906c49
Miaoqing Pan <miaoqing@codeaurora.org>
2016-09-21 10:54:37 +0800

    ar71xx: Add new profile IOE_TEST
    This profile only for IOE factory manufacture.
    
    Change-Id: I24e35af6de79e34cd0d62a56eba17dd4e7f2cb44
    Signed-off-by: Miaoqing Pan <miaoqing@codeaurora.org>

--- target/linux/ar71xx/generic/profiles/qsdk.mk
+++ target/linux/ar71xx/generic/profiles/qsdk.mk
@@ -52,5 +52,16 @@ define Profile/QSDK_IoE_DBPAN/Description
 	Enables WiFi 2.4G, 5G, Bluetooth open source packages
 endef
 
+define Profile/QSDK_IOE_TEST
+	NAME:=Qualcomm-Atheros SDK test Profile
+	PACKAGES:=$(IOE_BASE) $(TEST_TOOLS) $(WIFI_OPEN) $(BLUETOOTH) \
+		  kmod-usb-serial kmod-usb-serial-pl2303 kmod-art2
+endef
+
+define Profile/QSDK_IOE_TEST/Description
+	QSDK IoE test package set configuration.
+endef
+
 $(eval $(call Profile,QSDK_IOE_SB))
 $(eval $(call Profile,QSDK_IOE_DBPAN))
+$(eval $(call Profile,QSDK_IOE_TEST))
