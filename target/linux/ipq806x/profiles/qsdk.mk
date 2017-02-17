b5516c828ad08c51d5c58243ea8fa0f9e1e806f2
Naresh Kumar Mehta <naresh@codeaurora.org>
2017-02-17 19:31:22 +0530

    ipq806x: Remove streamboost3 from defalut premium profile
    streamboost3 build variant will continue to build using build script.
    
    Change-Id: I40fb0d3dc78d6632596d3e1a4f740174eb96ddc3
    Signed-off-by: Naresh Kumar Mehta <naresh@codeaurora.org>

--- target/linux/ipq806x/profiles/qsdk.mk
+++ target/linux/ipq806x/profiles/qsdk.mk
@@ -165,7 +165,7 @@ define Profile/QSDK_Premium
 		$(NETWORKING) $(UTILS) $(SHORTCUT_FE) $(HW_CRYPTO) $(QCA_RFS) \
 		$(AUDIO) $(VIDEO) $(IGMPSNOOING_RSTP) $(IPSEC) $(QOS) $(QCA_ECM_PREMIUM) \
 		$(NSS_MACSEC) $(TEST_TOOLS) $(NSS_CRYPTO) $(NSS_CLIENTS) $(COREBSP_UTILS) \
-		$(MAP_PKGS) $(HYFI) $(AQ_PHY) $(FAILSAFE) kmod-art2 streamboost3
+		$(MAP_PKGS) $(HYFI) $(AQ_PHY) $(FAILSAFE) kmod-art2
 endef
 
 define Profile/QSDK_Premium/Description
