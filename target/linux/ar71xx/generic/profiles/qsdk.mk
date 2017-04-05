0dcf9d0d764865430c132c36009d1c757cbf1c84
Miaoqing Pan <miaoqing@codeaurora.org>
2017-04-05 14:04:42 +0800

    ar71xx: add ioe premium profile to support qca-wifi
    Change-Id: I2aed6a9ce1e9b9e363453eb37ef3c7482f846fb9
    Signed-off-by: Zhi Chen <zhichen@codeaurora.org>
    Signed-off-by: Miaoqing Pan <miaoqing@codeaurora.org>

--- target/linux/ar71xx/generic/profiles/qsdk.mk
+++ target/linux/ar71xx/generic/profiles/qsdk.mk
@@ -29,6 +29,14 @@ BLUETOPIA:=kmod-bluetopia-usb-driver bluetopia
 
 BLUETOOTH:=$(BLUETOPIA)
 
+WIFI_10_4:=kmod-qca-wifi-10.4-unified-perf kmod-art2 \
+        qca-hostap-10.4 qca-hostapd-cli-10.4 qca-wpa-supplicant-10.4 \
+        qca-wpa-cli-10.4 qca-wapid qca-wpc-10.4 \
+        qca-acfg-10.4 qca-wrapd-10.4 qca-spectral-10.4 qcmbr-10.4 whc whc-ui \
+        qca-wifi-fw-hw3-10.4-asic qca-wifi-fw-hw6-10.4-asic \
+        qca-wifi-fw-hw9-10.4-asic qca-wifi-fw-hw10-10.4-asic \
+        qca-iface-mgr-10.4
+
 define Profile/QSDK_IOE_SB
 	NAME:=Qualcomm-Atheros SDK IoE Single Band Profile
 	PACKAGES:=$(IOE_BASE) $(TEST_TOOLS) $(ALLJOYN) $(WIFI_OPEN) \
@@ -56,6 +64,19 @@ define Profile/QSDK_IOE_DBPAN/Description
 	Enables WiFi 2.4G, 5G, Bluetooth open source packages
 endef
 
+define Profile/QSDK_IOE_PREMIUM
+	NAME:=Qualcomm-Atheros SDK IoE Dual Band and Personal Area Network Profile
+	PACKAGES:=$(IOE_BASE) $(TEST_TOOLS) $(ALLJOYN) $(WIFI_10_4) $(BLUETOOTH) \
+		  qca-legacy-uboot-cus531mp3-16M qca-legacy-uboot-cus531mp3-32M \
+		  qca-legacy-uboot-cus531mp3-dual qca-legacy-uboot-cus531mp3-nand \
+		  kmod-usb-serial kmod-usb-serial-pl2303
+endef
+
+define Profile/QSDK_IOE_PREMIUM/Description
+	QSDK IoE premium package set configuration.
+	Enables WiFi 2.4G, 5G, Bluetooth open source packages
+endef
+
 define Profile/QSDK_IOE_TEST
 	NAME:=Qualcomm-Atheros SDK IoE test Profile
 	PACKAGES:=$(IOE_BASE) $(TEST_TOOLS) $(WIFI_OPEN) kmod-bluetooth  \
@@ -69,4 +90,5 @@ endef
 
 $(eval $(call Profile,QSDK_IOE_SB))
 $(eval $(call Profile,QSDK_IOE_DBPAN))
+$(eval $(call Profile,QSDK_IOE_PREMIUM))
 $(eval $(call Profile,QSDK_IOE_TEST))
