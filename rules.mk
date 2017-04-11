0b80db3ae5cf0e87c3a6cfd1c25e2b986cc47810
Naresh Kumar Mehta <naresh@codeaurora.org>
2017-04-11 15:52:35 +0530

    ipq806x: Fix build when using external toolchain
    elfutils - Change dependency to be universally applicable for argp-standalone
    iputils  - Change dependency to be universally applicable for libopenssl
    rules.mk - Have Wno-error=unused-but-set-variable & Wno-error=unused-result
               to be universally applicable
    
    Change-Id: Ie5c38efb48e67d4ad8eadb452f9f1750381c1b7d
    Signed-off-by: Naresh Kumar Mehta <naresh@codeaurora.org>

--- rules.mk
+++ rules.mk
@@ -139,12 +139,12 @@ ifeq ($(CONFIG_ARCH_64BIT),y)
   LIB_SUFFIX:=64
 endif
 
+TARGET_CFLAGS+= -fhonour-copts $(if $(CONFIG_GCC_VERSION_4_4)$(CONFIG_GCC_VERSION_4_5),,-Wno-error=unused-but-set-variable) -Wno-error=unused-result
 ifndef DUMP
   ifeq ($(CONFIG_EXTERNAL_TOOLCHAIN),)
     -include $(TOOLCHAIN_DIR)/info.mk
     export GCC_HONOUR_COPTS:=0
     TARGET_CROSS:=$(if $(TARGET_CROSS),$(TARGET_CROSS),$(OPTIMIZE_FOR_CPU)-openwrt-linux$(if $(TARGET_SUFFIX),-$(TARGET_SUFFIX))-)
-    TARGET_CFLAGS+= -fhonour-copts $(if $(CONFIG_GCC_VERSION_4_4)$(CONFIG_GCC_VERSION_4_5),,-Wno-error=unused-but-set-variable) -Wno-error=unused-result
     TARGET_CPPFLAGS+= -I$(TOOLCHAIN_DIR)/usr/include -I$(TOOLCHAIN_DIR)/include
     TARGET_LDFLAGS+= -L$(TOOLCHAIN_DIR)/usr/lib -L$(TOOLCHAIN_DIR)/lib
     TARGET_PATH:=$(TOOLCHAIN_DIR)/bin:$(TARGET_PATH)
