afef69f8692bad48fcf1e4d54a771f201614d000
xiaofeis <xiaofeis@codeaurora.org>
2017-07-25 19:55:06 -0700

    ag71xx: Set carrier off before interface link up
    Change-Id: Ic9f1750f44d55d5e75d5a20acd432684a7d3b438
    Signed-off-by: xiaofeis <xiaofeis@codeaurora.org>

--- target/linux/ar71xx/files/drivers/net/ethernet/atheros/ag71xx/ag71xx_main.c
+++ target/linux/ar71xx/files/drivers/net/ethernet/atheros/ag71xx/ag71xx_main.c
@@ -1945,6 +1945,7 @@ static int __devinit ag71xx_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "unable to register net device\n");
 		goto err_free_desc;
 	}
+	netif_carrier_off(dev);
 
 	pr_info("%s: Atheros AG71xx at 0x%08lx, irq %d\n",
 		dev->name, dev->base_addr, dev->irq);
