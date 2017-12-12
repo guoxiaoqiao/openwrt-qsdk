42eaa2a0a6c0e8d4b6ab3c1ca1af8d65baa144ae
Ram Chandra Jangir <rjangir@codeaurora.org>
2017-12-12 12:21:07 +0530

    scripts: Fix 'git describe' failure during build
    With current builds git produces the below message
    "fatal: No names found, cannot describe anything"
    under certain conditions (for example, using git
    describe when annotated tags are not available, etc).
    
    This change adds '--always' too to show uniquely
    abbreviated commit object as fallback, also removes
    the references of 15.05.1 since our tags are having
    different names.
    
    Change-Id: I3ec435a00fb1f1e8b69ae199d323a5b61e1a418c
    Signed-off-by: Ram Chandra Jangir <rjangir@codeaurora.org>

--- scripts/getver.sh
+++ scripts/getver.sh
@@ -18,8 +18,8 @@ try_svn() {
 
 try_git() {
 	git rev-parse --git-dir >/dev/null 2>&1 || return 1
-	REV="$(git describe --tags | sed "s/v15.05.1-\([0-9]*\)-.*/\1/g")"
-	REV="${REV:+r$((REV+49254))}"
+	REV="$(git describe --always --tags | sed 's/.*-g//g')"
+	REV="${REV:+$REV+r49254}"
 	[ -n "$REV" ]
 }
 
