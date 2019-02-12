6d7f7a64549e4ac8297484dc0117a6424681963d
Antony Arun T <antothom@codeaurora.org>
2019-02-12 17:45:38 +0530

    ipq: Enable uboot-2016 for ipq6018
    Change-Id: I6691aaff51bb91379515bf6386626c7d7c3c4086
    Signed-off-by: Antony Arun T <antothom@codeaurora.org>

--- target/linux/ipq/ipq60xx/target.mk
+++ target/linux/ipq/ipq60xx/target.mk
@@ -4,7 +4,7 @@ BOARDNAME:=QCA IPQ60xx(32bit) based boards
 CPU_TYPE:=cortex-a7
 
 DEFAULT_PACKAGES += \
-	u-boot-ipq6018 sysupgrade-helper kmod-usb-phy-ipq807x
+	uboot-2016-ipq6018 sysupgrade-helper kmod-usb-phy-ipq807x
 
 define Target/Description
 	Build firmware image for IPQ60xx SoC devices.
