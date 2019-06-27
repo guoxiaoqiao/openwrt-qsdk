39cb2822c460efe479637c5c4cbb84e78d2c7baf
Sanjeev N <nsanjeev@codeaurora.org>
2019-06-27 17:40:13 +0530

    ipq: enable fwupgrade-tools for 32 bit target
    For all 32 bit targets, use fwupgrade-tools to install dumpimage
    and mkimage on rootfs. Remove sysupgrade-helper since the binaries
    are avaiable as part of uboot-2016 compilation.
    
    Change-Id: I97eeb8a468b8434dda3b7e91e428017ac56f39fb
    Signed-off-by: Sanjeev N <nsanjeev@codeaurora.org>

--- target/linux/ipq/ipq60xx/target.mk
+++ target/linux/ipq/ipq60xx/target.mk
@@ -4,7 +4,7 @@ BOARDNAME:=QCA IPQ60xx(32bit) based boards
 CPU_TYPE:=cortex-a7
 
 DEFAULT_PACKAGES += \
-	uboot-2016-ipq6018 sysupgrade-helper kmod-usb-phy-ipq807x \
+	uboot-2016-ipq6018 fwupgrade-tools kmod-usb-phy-ipq807x \
 	kmod-usb-dwc3-qcom
 
 define Target/Description
