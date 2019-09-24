a9abf6dc70f3adbf0b9b743bc1ed0268c49ee002
Naresh Kumar Mehta <naresh@codeaurora.org>
2019-09-24 12:08:00 +0530

    ipq807x: Enable qrtr instead of qca-qrtr
    For Open profile
    
    Change-Id: I9ea1dc1935fc3774024037c350fcb6fe430ca036
    Signed-off-by: Naresh Kumar Mehta <naresh@codeaurora.org>

--- target/linux/ipq/profiles/qsdk.mk
+++ target/linux/ipq/profiles/qsdk.mk
@@ -202,7 +202,7 @@ define Profile/QSDK_Open
 	$(WIFI_OPEN_PKGS) $(STORAGE) $(USB_ETHERNET) $(UTILS) $(NETWORKING) \
 	$(TEST_TOOLS) $(COREBSP_UTILS) $(KPI) $(SHORTCUT_FE) $(EXTRA_NETWORKING) \
 	$(USB_DIAG) $(FTM) $(CNSS_DIAG) qca-cnss-daemon qca-wifi-hk-fw-hw1-10.4-asic \
-	$(QRTR) $(QMI_SAMPLE_APP) $(FAILSAFE) ath11k-fwtest ath11k-qdss
+	qrtr $(QMI_SAMPLE_APP) $(FAILSAFE) ath11k-fwtest ath11k-qdss
 endef
 
 define Profile/QSDK_Open/Description
