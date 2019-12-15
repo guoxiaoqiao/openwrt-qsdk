c83b1f837a68bf5f01ccff1e880003eff37701f2
Amandeep Singh <amansing@codeaurora.org>
2019-12-15 18:05:27 +0530

    ipq50xx: Update mproc dependencies
    Update mproc dependencies to enable mproc for ipq50xx.
    
    Change-Id: If058b798cb67f0bbb7fbb643c7b5323ff3ecf23e
    Signed-off-by: Amandeep Singh <amansing@codeaurora.org>

--- target/linux/ipq/modules.mk
+++ target/linux/ipq/modules.mk
@@ -140,7 +140,7 @@ $(eval $(call KernelPackage,usb-phy-ipq807x))
 
 define KernelPackage/qrtr_mproc
   TITLE:= Ath11k Specific kernel configs for IPQ807x and IPQ60xx
-  DEPENDS+= @TARGET_ipq_ipq807x||TARGET_ipq_ipq807x_64||TARGET_ipq_ipq60xx||TARGET_ipq_ipq60xx_64
+  DEPENDS+= @TARGET_ipq_ipq807x||TARGET_ipq_ipq807x_64||TARGET_ipq_ipq60xx||TARGET_ipq_ipq60xx_64||TARGET_ipq_ipq50xx||TARGET_ipq_ipq50xx_64
   KCONFIG:= \
 	  CONFIG_QRTR=y \
 	  CONFIG_QCOM_APCS_IPC=y \
