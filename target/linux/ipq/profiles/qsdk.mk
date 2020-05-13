a21fe74c3cfb1062bc2cee71d6134d84ac149548
Amandeep Singh <amansing@codeaurora.org>
2020-05-13 17:32:48 +0530

    Revert "ipq: Enable BT IPC driver for premium profile"
    This reverts commit 3b9cf6c5cace060fe74a2f507fb0ef013da3322c.
    This is needed to be enabled as per the target.
    
    Signed-off-by: Amandeep Singh <amansing@codeaurora.org>
    Change-Id: Ie180b555aa62be8a9effa46d06f80a49be5d3fe1

--- target/linux/ipq/profiles/qsdk.mk
+++ target/linux/ipq/profiles/qsdk.mk
@@ -217,8 +217,6 @@ QMI_SAMPLE_APP:=kmod-qmi_sample_client
 
 MHI_QRTR:=kmod-mhi-qrtr-mproc
 
-BT_IPC:=kmod-bt_tty
-
 ifneq ($(LINUX_VERSION),3.18.21)
 	EXTRA_NETWORKING:=$(NSS_COMMON) $(QCA_EDMA) $(NSS_STANDARD) $(CD_ROUTER) -lacpd \
 	$(HW_CRYPTO) $(QCA_RFS) $(AUDIO) $(VIDEO) -rstp \
@@ -253,7 +251,7 @@ define Profile/QSDK_Premium
 		$(MAP_PKGS) $(HYFI) $(QCA_MAD) $(AQ_PHY) $(FAILSAFE) kmod-art2 -lacpd $(USB_DIAG) \
 		$(QCA_LITHIUM) $(NSS_EIP197_FW) $(CNSS_DIAG) $(FTM) $(QMSCT_CLIENT) \
 		$(MHI_QRTR) $(KPI) $(QRTR) $(NSS_USERSPACE) \
-		$(NSS_RMNET) $(BT_IPC)
+		$(NSS_RMNET)
 endef
 
 define Profile/QSDK_Premium/Description
