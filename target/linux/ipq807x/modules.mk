e50b007785afd868d8d0ec29df481323d3434946
Sivaprakash Murugesan <sivaprak@codeaurora.org>
2020-05-21 13:06:19 +0530

    config-5.4: Move msm-mproc out of openwrt
    msm-mproc config needs to be moved out of openwert and should be added
    in platform specific Makefile
    
    Signed-off-by: Sivaprakash Murugesan <sivaprak@codeaurora.org>
    Change-Id: Ib5fc1a52eef2545c6888bb4456836e9c8e6ebc15

--- target/linux/ipq807x/modules.mk
+++ target/linux/ipq807x/modules.mk
@@ -18,31 +18,4 @@ endef
 
 $(eval $(call KernelPackage,usb-phy-ipq807x))
 
-define KernelPackage/msm-mproc
-  TITLE:= Default kernel configs
-  DEPENDS+= @TARGET_ipq_ipq807x||TARGET_ipq_ipq807x_64||TARGET_ipq_ipq60xx||TARGET_ipq_ipq60xx_64||TARGET_ipq807x
-  KCONFIG:= \
-	  CONFIG_QRTR=y \
-	  CONFIG_QCOM_APCS_IPC=y \
-	  CONFIG_QCOM_GLINK_SSR=y \
-	  CONFIG_QCOM_Q6V5_WCSS=y \
-	  CONFIG_MSM_RPM_RPMSG=y \
-	  CONFIG_RPMSG_QCOM_GLINK_RPM=y \
-	  CONFIG_REGULATOR_RPM_GLINK=y \
-	  CONFIG_IPQ_SUBSYSTEM_RAMDUMP=y \
-	  CONFIG_QCOM_SYSMON=y \
-	  CONFIG_RPMSG=y \
-	  CONFIG_RPMSG_CHAR=y \
-	  CONFIG_RPMSG_QCOM_GLINK_SMEM=y \
-	  CONFIG_RPMSG_QCOM_SMD=y \
-	  CONFIG_QRTR_SMD=y \
-	  CONFIG_QCOM_Q6V5_SSR=y \
-	  CONFIG_MAILBOX=y \
-	  CONFIG_DIAG_OVER_QRTR=y
-endef
 
-define KernelPackage/msm-mproc/description
-Default kernel configs.
-endef
-
-$(eval $(call KernelPackage,msm-mproc))
