ac7186ddcef9ab10791d8ba0002752b45f82820b
Murat Sezgin <msezgin@codeaurora.org>
2020-06-29 17:51:59 -0700

    qsdk: Enable nathelper-extra and its dependency
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>
    Change-Id: Icbf1f25b9cf6d91edd0e799d69b1aefe199beebf
    Signed-off-by: Sandhya KS <sandhyks@codeaurora.org>

--- target/linux/ipq807x/profiles/qsdk.mk
+++ target/linux/ipq807x/profiles/qsdk.mk
@@ -72,8 +72,9 @@ FAILSAFE:= kmod-bootconfig
 
 NETWORKING:=mcproxy -dnsmasq dnsmasq-dhcpv6 bridge ip-full trace-cmd mwan3 \
 	rp-pppoe-relay iptables-mod-extra iputils-tracepath iputils-tracepath6 \
-	luci-app-upnp luci-app-ddns luci-proto-ipv6
-#	kmod-nf-nathelper-extra kmod-ipt-nathelper-rtsp \
+	luci-app-upnp luci-app-ddns luci-proto-ipv6 \
+	kmod-nf-nathelper-extra kmod-nf-nathelper
+#	kmod-ipt-nathelper-rtsp \
 
 CD_ROUTER:=kmod-ipt-ipopt kmod-bonding kmod-nat-sctp lacpd \
 	arptables ds-lite 6rd ddns-scripts xl2tpd \
