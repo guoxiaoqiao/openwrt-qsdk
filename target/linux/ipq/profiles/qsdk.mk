030778defd47e3a8bfcc67497375e690e0422668
Mainak Sen <smainak@codeaurora.org>
2020-08-25 16:06:22 +0530

    ipq: config: Enable qca-nsmscs module.
    Signed-off-by: Mainak Sen <smainak@codeaurora.org>
    Change-Id: I96f88a16a42443af6a25832c9dee52f1d4f1c5cb

--- target/linux/ipq/profiles/qsdk.mk
+++ target/linux/ipq/profiles/qsdk.mk
@@ -48,13 +48,13 @@ NSS_CLIENTS_STANDARD:= kmod-qca-nss-drv-qdisc kmod-qca-nss-drv-igs kmod-qca-nss-
 	kmod-qca-nss-drv-map-t kmod-qca-nss-drv-lag-mgr \
 	kmod-qca-nss-drv-bridge-mgr kmod-qca-nss-drv-gre kmod-qca-nss-drv-pppoe \
 	kmod-qca-nss-drv-ovpn-mgr kmod-qca-nss-drv-ovpn-link kmod-qca-nss-drv-vxlanmgr kmod-qca-nss-drv-netlink \
-	kmod-qca-ovsmgr kmod-qca-nss-drv-match kmod-qca-nss-drv-mirror
+	kmod-qca-ovsmgr kmod-qca-nss-drv-match kmod-qca-nss-drv-mirror kmod-qca-nss-drv-mscs
 
 NSS_CLIENTS_256MB:= kmod-qca-nss-drv-bridge-mgr kmod-qca-nss-drv-pppoe
 
 NSS_CLIENTS_ENTERPRISE:= kmod-qca-nss-drv-qdisc kmod-qca-nss-drv-profile \
 	kmod- kmod-qca-nss-drv-bridge-mgr kmod-qca-nss-drv-netlink kmod-qca-nss-drv-tlsmgr \
-	kmod-qca-nss-drv-match kmod-qca-nss-drv-mirror
+	kmod-qca-nss-drv-match kmod-qca-nss-drv-mirror kmod-qca-nss-drv-mscs
 
 NSS_CRYPTO:= kmod-qca-nss-crypto kmod-qca-nss-cfi-cryptoapi kmod-qca-nss-cfi-ocf kmod-qca-nss-drv-ipsecmgr kmod-crypto-ocf kmod-qca-nss-drv-ipsecmgr-klips
 
