751fe0acee7542c0f9591a1b33a8ec0fa2f31f45
Naresh Parati <cnparat@codeaurora.org>
2020-08-26 10:34:32 +0530

    qsdk.mk: Enable iface-mgr package
    Change-Id: I0b5990d4d2c335a0f76549a82af8f67a461784a0
    Signed-off-by: Naresh Parati <cnparat@codeaurora.org>

--- target/linux/ipq807x/profiles/qsdk.mk
+++ target/linux/ipq807x/profiles/qsdk.mk
@@ -39,10 +39,10 @@ SWITCH_SSDK_PKGS:= kmod-qca-ssdk-hnat kmod-qca-ssdk-nohnat qca-ssdk-shell swconf
 WIFI_PKGS:=kmod-qca-wifi-unified-profile \
 	qca-hostap qca-hostapd-cli qca-wpa-supplicant \
 	qca-wpa-cli qca-cfg80211tool qca-wifi-scripts \
-	qca-acfg qca-wrapd athtestcmd-lith
+	qca-acfg qca-wrapd athtestcmd-lith qca-iface-mgr-10.4
 #	qca-spectral qca-wpc sigma-dut \
 #	qcmbr-10.4 qca-wapid whc whc-ui \
-#	qca-lowi qca-iface-mgr-10.4 qca-icm qca-cfg80211 athdiag qca-cnss-daemon \
+#	qca-lowi qca-icm qca-cfg80211 athdiag qca-cnss-daemon \
 
 WIFI_FW_PKGS:=qca-wifi-hk-fw-hw1-10.4-asic
 
