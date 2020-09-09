ecb4006b088de10719b7d533997e9ab924028e70
Ken Zhu <guigenz@codeaurora.org>
2020-09-09 15:50:08 -0700

    ipq807x: enable 464xlat
    add 464xlat support in the premium profile
    
    Change-Id: Ia86d20f3bdb329e414bf67a5eae261d122339dc1
    Signed-off-by: Ken Zhu <guigenz@codeaurora.org>

--- target/linux/ipq807x/profiles/qsdk.mk
+++ target/linux/ipq807x/profiles/qsdk.mk
@@ -77,7 +77,7 @@ CD_ROUTER:=kmod-ipt-ipopt kmod-bonding kmod-nat-sctp lacpd \
 QOS:=tc kmod-sched kmod-sched-core kmod-sched-connmark kmod-ifb iptables \
 	iptables-mod-filter iptables-mod-ipopt iptables-mod-conntrack-extra
 
-MAP_PKGS:=-map-t -464xlat tayga
+MAP_PKGS:=-map-t 464xlat tayga
 
 HYFI:=hyfi hyfi-ui
 
