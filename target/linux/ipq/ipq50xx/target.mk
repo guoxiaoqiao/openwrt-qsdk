32687c01aaefa1a82dd11cf1ff1a25e905ebad57
Vandhiadevan Karunamoorthy <vkarunam@codeaurora.org>
2020-09-15 12:45:57 +0530

    target: ipq50xx: Add uboot-2016-ipq5018_tiny_debug support
    This change add uboot-2016-ipq5018_tiny_debug in default
    build support
    
    Change-Id: I359eba0b22485e48dd347c5438009504ec50e4bd
    Signed-off-by: Vandhiadevan Karunamoorthy <vkarunam@codeaurora.org>

--- target/linux/ipq/ipq50xx/target.mk
+++ target/linux/ipq/ipq50xx/target.mk
@@ -4,7 +4,7 @@ BOARDNAME:=QCA IPQ50XX(32bit) based boards
 CPU_TYPE:=cortex-a7
 
 DEFAULT_PACKAGES += \
-	uboot-2016-ipq5018 uboot-2016-ipq5018_tiny fwupgrade-tools \
+	uboot-2016-ipq5018 uboot-2016-ipq5018_tiny uboot-2016-ipq5018_tiny_debug fwupgrade-tools \
 	kmod-usb-phy-ipq5018 kmod-usb-dwc3-qcom kmod-bt_tty kmod-clk-test
 
 define Target/Description
