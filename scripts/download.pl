cb83d726857c24239f93d0c17db25460a5c125e3
Ram Chandra Jangir <rjangir@codeaurora.org>
2020-10-09 13:02:25 +0530

    scripts: Add support to use exported NFS_MIRROR_SERVER
    This change adds support to use an export variable 'NFS_MIRROR_SERVER'
    for NFS path & if export variable is configured, it will use that as
    NFS path, else default “/prj/qct/openwrt/caf_mirrored_tarballs” will
    be used.
    
    Change-Id: I4ac5920ae7c1e944a7fcd147171039439aebeb03
    Signed-off-by: Ram Chandra Jangir <rjangir@codeaurora.org>

--- scripts/download.pl
+++ scripts/download.pl
@@ -24,6 +24,7 @@ my $scriptdir = dirname($0);
 my @mirrors;
 my $ok;
 my $is_network_down = $ENV{'IS_NETWORK_DOWN'};
+my $nfs_server = $ENV{'NFS_MIRROR_SERVER'};
 
 $url_filename or $url_filename = $filename;
 
@@ -266,7 +267,12 @@ push @mirrors, 'https://sources.openwrt.org';
 push @mirrors, 'https://mirror2.openwrt.org/sources';
 
 my @caf_mirrors;
-push @caf_mirrors, 'file:///prj/qct/openwrt/caf_mirrored_tarballs';
+if ($nfs_server) {
+	push @caf_mirrors, "file://${nfs_server}";
+} else {
+	push @caf_mirrors, 'file:///prj/qct/openwrt/caf_mirrored_tarballs';
+}
+
 if (!$is_network_down) {
 	push @caf_mirrors, 'https://source.codeaurora.org/mirrored_source/quic/qsdk';
 }
