77e3a4bc9cc7ed1a3238df4300d0a69663443e1f
Kathiravan T <kathirav@codeaurora.org>
2021-02-15 20:14:40 +0530

    ipq60xx_32: enable the uboot and lk package
    Signed-off-by: Kathiravan T <kathirav@codeaurora.org>
    Change-Id: Ic410ff5fbcc03c233f1cb0d8eaf30d5af2f44bbe

--- target/linux/ipq60xx/ipq60xx_32/target.mk
+++ target/linux/ipq60xx/ipq60xx_32/target.mk
@@ -5,7 +5,7 @@ BOARDNAME:=QTI IPQ60xx(32bit) based boards
 CPU_TYPE:=cortex-a7
 
 DEFAULT_PACKAGES += \
-	uboot-2016-ipq60xx uboot-2016-ipq60xx_tiny lk-ipq60xx \
+	uboot-2016-ipq6018 uboot-2016-ipq6018_tiny lk-ipq6018 \
 	kmod-usb-phy-ipq60xx kmod-usb-dwc3-qcom-internal \
 	fwupgrade-tools
 
