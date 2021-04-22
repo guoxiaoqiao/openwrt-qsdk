75aeefae032221d55ea4ad341b4dde230fe92581
Ram Chandra Jangir <rjangir@codeaurora.org>
2021-04-22 17:48:01 +0530

    ipq50xx: Enable diag support for ipq50xx
    This change enables support for diag in ipq50xx target.
    
    Change-Id: I898f3dfd3fe3995b4413b87a01fc048ea21d8149
    Signed-off-by: Ram Chandra Jangir <rjangir@codeaurora.org>

--- target/linux/ipq50xx/ipq50xx_32/target.mk
+++ target/linux/ipq50xx/ipq50xx_32/target.mk
@@ -6,7 +6,7 @@ CPU_TYPE:=cortex-a7
 
 DEFAULT_PACKAGES += \
 	uboot-2016-ipq5018 uboot-2016-ipq5018_tiny fwupgrade-tools \
-	kmod-usb-phy-ipq5018 kmod-usb-dwc3-qcom kmod-bt_tty kmod-clk-test
+	kmod-usb-phy-ipq5018 kmod-usb-dwc3-qcom-internal kmod-bt_tty kmod-clk-test
 
 define Target/Description
 	Build firmware image for IPQ50xx SoC devices.
