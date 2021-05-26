e7b2a669648bdb7df723fc462f2afdc8b7ad34b0
Ram Chandra Jangir <rjangir@codeaurora.org>
2021-05-26 15:42:02 +0530

    ipq: Enable e2fsprogs for 256M profile
    e2fsprogs provides mkfs.ext4 command which is required for
    eMMC overlay, hence we enable e2fsprogs for 256M profile.
    
    Change-Id: Id64d11c616a7c4fbfe8189ef698e3cce00fb9ca1
    Signed-off-by: Ram Chandra Jangir <rjangir@codeaurora.org>

--- target/linux/ipq/profiles/qsdk.mk
+++ target/linux/ipq/profiles/qsdk.mk
@@ -373,7 +373,7 @@ define Profile/QSDK_256
 		$(QCA_ECM_STANDARD) $(NSS_MACSEC) \
 		$(NSS_CLIENTS_256MB) $(HYFI) $(QCA_EZMESH) $(FAILSAFE) -lacpd \
 		$(QCA_LITHIUM) $(CNSS_DIAG) $(FTM) $(QMSCT_CLIENT) qca-wifi-cyp-fw-hw1-11.0-asic \
-		$(QRTR) $(MHI_QRTR) kmod-macvlan $(EMESH_SP)
+		$(QRTR) $(MHI_QRTR) kmod-macvlan $(EMESH_SP) e2fsprogs
 endef
 
 define Profile/QSDK_256/Description
